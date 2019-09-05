033a662dba7f7c776897129ba4330cbd78392e38
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-05 11:19:02 +0800

    Compressor.id

--- goblinframework-api/src/main/java/org/goblinframework/api/compression/Compressor.java
+++ goblinframework-api/src/main/java/org/goblinframework/api/compression/Compressor.java
@@ -2,14 +2,20 @@ package org.goblinframework.api.compression;
 
 public enum Compressor {
 
-  BZIP2("bzip2"),
-  GZIP("gz"),
-  DEFLATE("deflate");
+  BZIP2((byte) 1, "bzip2"),
+  GZIP((byte) 2, "gz"),
+  DEFLATE((byte) 3, "deflate");
 
+  private final byte id;
   private final String name;
 
-  Compressor(String name) {
+  Compressor(byte id, String name) {
+    this.id = id;
     this.name = name;
+  }
+
+  public byte getId() {
+    return id;
   }
 
   public String getName() {
