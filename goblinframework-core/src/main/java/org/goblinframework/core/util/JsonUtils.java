58f5951d9f2575e3799b5197e5be24a7f8ba6938
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-06 12:37:22 +0800

    org.goblinframework.core.util.JsonUtils

---
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/JsonUtils.java
@@ -0,0 +1,24 @@
+package org.goblinframework.core.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+final public class JsonUtils {
+
+  private static final ObjectMapper DEFAULT_OBJECT_MAPPER;
+
+  static {
+    DEFAULT_OBJECT_MAPPER = createObjectMapper();
+  }
+
+  public static ObjectMapper createObjectMapper() {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    mapper.configure(DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS, true);
+    mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    return mapper;
+  }
+}
