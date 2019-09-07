61f937893080ca21ea57be611eed0b96fa799db2
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-07 16:52:10 +0800

    org.goblinframework.core.event.dsl.GoblinCallbackTest

---
+++ goblinframework-core/src/test/java/org/goblinframework/core/event/dsl/GoblinCallbackTest.kt
@@ -0,0 +1,24 @@
+package org.goblinframework.core.event.dsl
+
+import org.apache.commons.lang3.RandomStringUtils
+import org.apache.commons.lang3.RandomUtils
+import org.goblinframework.core.event.GoblinEventBus
+import org.junit.Assert
+import org.junit.Test
+
+class GoblinCallbackTest {
+
+  @Test
+  fun testCallback() {
+    val s = RandomStringUtils.randomAlphanumeric(64)
+    Assert.assertEquals(s, echo(s))
+    val i = RandomUtils.nextInt()
+    Assert.assertEquals(i, echo(i))
+
+    Assert.assertNull(echo(null))
+  }
+
+  private fun echo(input: Any?): Any? {
+    return GoblinEventBus.execute { input }.get()
+  }
+}
