3a336d417bd139ba392f18994b5b467d494cb9e5
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-07 15:21:20 +0800

    org.goblinframework.core.lifecycle.GoblinModuleException

---
+++ goblinframework-core/src/main/java/org/goblinframework/core/lifecycle/GoblinModuleException.kt
@@ -0,0 +1,16 @@
+package org.goblinframework.core.lifecycle
+
+class GoblinModuleException : RuntimeException {
+
+  constructor() {}
+
+  constructor(message: String) : super(message) {}
+
+  constructor(message: String, cause: Throwable) : super(message, cause) {}
+
+  constructor(cause: Throwable) : super(cause) {}
+
+  companion object {
+    private const val serialVersionUID = -528572139800126784L
+  }
+}
