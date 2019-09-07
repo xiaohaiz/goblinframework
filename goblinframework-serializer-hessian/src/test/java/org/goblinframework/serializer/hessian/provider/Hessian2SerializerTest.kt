1cac96f7dae6ee97f1e4e782e05bd25eb2087f83
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-07 18:05:55 +0800

    org.goblinframework.serializer.hessian.provider.Hessian2SerializerTest

--- goblinframework-serializer-hessian/src/test/java/org/goblinframework/serializer/hessian/provider/Hessian2SerializerTest.kt
+++ goblinframework-serializer-hessian/src/test/java/org/goblinframework/serializer/hessian/provider/Hessian2SerializerTest.kt
@@ -1,15 +1,31 @@
 package org.goblinframework.serializer.hessian.provider
 
+import org.goblinframework.api.serialization.Serializer
+import org.goblinframework.serialization.core.SerializationManager
 import org.goblinframework.test.runner.GoblinTestRunner
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.test.context.ContextConfiguration
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.time.Instant
 
 @RunWith(GoblinTestRunner::class)
 @ContextConfiguration("/UT.xml")
 class Hessian2SerializerTest {
 
   @Test
-  fun hessian2Serializer() {
+  fun testInstant() {
+    val i = Instant.now()
+    val t = i.toEpochMilli()
+    val s = SerializationManager.INSTANCE.getSerialization(Serializer.HESSIAN2)!!
+    val bos = ByteArrayOutputStream()
+    s.serialize(i, bos)
+    val bis = ByteArrayInputStream(bos.toByteArray())
+    bos.close()
+    val d = s.deserialize(bis) as Instant
+    bis.close()
+    Assert.assertEquals(t, d.toEpochMilli())
   }
 }
