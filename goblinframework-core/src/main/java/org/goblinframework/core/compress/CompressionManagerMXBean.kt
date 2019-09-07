7664631bae69f2e804e2acb772cf2dbf10d6aa44
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-07 23:12:57 +0800

    Remove unnecessary classes.

--- goblinframework-core/src/main/java/org/goblinframework/core/compress/CompressionManagerMXBean.kt
+++
@@ -1,9 +0,0 @@
-package org.goblinframework.core.compress
-
-import java.lang.management.PlatformManagedObject
-
-interface CompressionManagerMXBean : PlatformManagedObject {
-
-  fun getCompressionList(): Array<CompressionMXBean>
-
-}
--- goblinframework-core/src/main/java/org/goblinframework/core/compression/CompressionUtils.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/compression/CompressionUtils.java
@@ -4,7 +4,6 @@ import org.apache.commons.compress.compressors.CompressorInputStream;
 import org.apache.commons.compress.compressors.CompressorOutputStream;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
 import org.apache.commons.compress.utils.IOUtils;
-import org.goblinframework.api.compression.Compressor;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.InputStream;
@@ -16,32 +15,6 @@ import java.io.OutputStream;
 final public class CompressionUtils {
 
   private static final CompressorStreamFactory FACTORY = new CompressorStreamFactory();
-
-  public static void compress(@NotNull Compressor compressor, @NotNull InputStream inStream, @NotNull OutputStream outStream) {
-    CompressorOutputStream cos = null;
-    try {
-      cos = FACTORY.createCompressorOutputStream(compressor.getAlgorithm(), outStream);
-      IOUtils.copy(inStream, cos);
-    } catch (Exception ex) {
-      throw CompressionException.newInstance(ex);
-    } finally {
-      IOUtils.closeQuietly(cos);
-    }
-  }
-
-  public static void decompress(@NotNull Compressor compressor,
-                                @NotNull InputStream inStream,
-                                @NotNull OutputStream outStream) {
-    CompressorInputStream cis = null;
-    try {
-      cis = FACTORY.createCompressorInputStream(compressor.getAlgorithm(), inStream);
-      IOUtils.copy(cis, outStream);
-    } catch (Exception ex) {
-      throw CompressionException.newInstance(ex);
-    } finally {
-      IOUtils.closeQuietly(cis);
-    }
-  }
 
   public static void compress(@NotNull CompressorMode mode,
                               @NotNull InputStream inStream,
