ed34e48e7cf9486bcf8f7bb758bac5a16c435539
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-08 11:52:09 +0800

    More modules.

--- goblinframework-core/src/main/java/org/goblinframework/core/bootstrap/GoblinModuleDefinition.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/bootstrap/GoblinModuleDefinition.kt
@@ -4,8 +4,13 @@ object GoblinModuleDefinition {
 
   val moduleNames = listOf(
       "CORE",
+      "MONITOR",
       "SERIALIZATION",
+      "WEBMVC",
       "EMBEDDED",
+      "CACHE",
+      "TRANSPORT",
+      "MANAGEMENT",
       "SYSTEM")
 
 }
