55595117d331cfc4f3135c9b2e03705174bf18c9
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-08 16:15:25 +0800

    org.goblinframework.core.util.NetworkUtils.getHostAddresses

--- goblinframework-core/src/main/java/org/goblinframework/core/util/NetworkUtils.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/NetworkUtils.java
@@ -1,8 +1,16 @@
 package org.goblinframework.core.util;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 final public class NetworkUtils {
 
@@ -19,5 +27,41 @@ final public class NetworkUtils {
         && !"0.0.0.0".equals(name)
         && !"127.0.0.1".equals(name)
         && IP_PATTERN.matcher(name).matches());
+  }
+
+  private static List<String> hostAddresses = null;
+
+  @NotNull
+  public synchronized static List<String> getHostAddresses() {
+    if (hostAddresses == null) {
+      try {
+        hostAddresses = determineHostAddresses();
+      } catch (Exception ex) {
+        throw new GoblinNetworkException(ex);
+      }
+    }
+    return hostAddresses;
+  }
+
+  @NotNull
+  private static List<String> determineHostAddresses() throws Exception {
+    List<InetAddress> list = new ArrayList<>();
+    Enumeration<NetworkInterface> it = NetworkInterface.getNetworkInterfaces();
+    if (it == null) {
+      return Collections.emptyList();
+    }
+    while (it.hasMoreElements()) {
+      Enumeration<InetAddress> addresses = it.nextElement().getInetAddresses();
+      while (addresses.hasMoreElements()) {
+        InetAddress address = addresses.nextElement();
+        if (isValidAddress(address)) {
+          list.add(address);
+        }
+      }
+    }
+    return list.stream()
+        .map(InetAddress::getHostAddress)
+        .distinct()
+        .collect(Collectors.toList());
   }
 }
