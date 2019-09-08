5707d6e861027acf5e0ed84a8643e256dd5a3e8c
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-08 21:42:33 +0800

    org.goblinframework.core.util.StringUtils

--- goblinframework-core/src/main/java/org/goblinframework/core/util/StringUtils.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/StringUtils.java
@@ -6,12 +6,47 @@ import java.util.function.Supplier;
 
 final public class StringUtils {
 
-  public static boolean isBlank(final CharSequence cs) {
-    return org.apache.commons.lang3.StringUtils.isBlank(cs);
+  public static boolean contains(final CharSequence seq, final CharSequence searchSeq) {
+    return org.apache.commons.lang3.StringUtils.contains(seq, searchSeq);
   }
 
   public static <T extends CharSequence> T defaultIfBlank(final T str, final T defaultStr) {
     return org.apache.commons.lang3.StringUtils.defaultIfBlank(str, defaultStr);
+  }
+
+  public static boolean endsWith(final CharSequence str, final CharSequence suffix) {
+    return org.apache.commons.lang3.StringUtils.endsWith(str, suffix);
+  }
+
+  public static boolean isBlank(final CharSequence cs) {
+    return org.apache.commons.lang3.StringUtils.isBlank(cs);
+  }
+
+  public static String replace(final String text, final String searchString, final String replacement) {
+    return org.apache.commons.lang3.StringUtils.replace(text, searchString, replacement);
+  }
+
+  // ==========================================================================
+  // EXTENSION UTILITIES METHODS
+  // ==========================================================================
+
+  public static String compactContinuousSlashes(final String path) {
+    if (path == null) {
+      return null;
+    }
+    String s = path;
+    if (contains(s, "//")) {
+      while (contains(s, "//")) {
+        s = replace(s, "//", "/");
+      }
+    }
+    if ("/".equals(s)) {
+      return "/";
+    }
+    if (endsWith(s, "/")) {
+      s = s.substring(0, s.length() - 1);
+    }
+    return s;
   }
 
   public static <T extends CharSequence> T defaultIfBlank(final T str, @NotNull final Supplier<T> defaultStrSupplier) {
---
+++ goblinframework-embedded/src/main/java/org/goblinframework/embedded/core/provider/JdkHttpHandler.java
@@ -0,0 +1,14 @@
+package org.goblinframework.embedded.core.provider;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class JdkHttpHandler implements HttpHandler {
+
+  @Override
+  public void handle(HttpExchange httpExchange) throws IOException {
+
+  }
+}
