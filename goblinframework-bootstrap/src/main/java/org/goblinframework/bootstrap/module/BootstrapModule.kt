d656dda9d2a15322c7ecd23e05127ecc3bd64c79
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-09 17:54:25 +0800

    org.goblinframework.bootstrap.module.BootstrapModule

---
+++ goblinframework-bootstrap/src/main/java/org/goblinframework/bootstrap/module/BootstrapModule.kt
@@ -0,0 +1,11 @@
+package org.goblinframework.bootstrap.module
+
+import org.goblinframework.core.bootstrap.GoblinModule
+
+class BootstrapModule : GoblinModule {
+
+  override fun name(): String {
+    return "BOOTSTRAP"
+  }
+
+}
