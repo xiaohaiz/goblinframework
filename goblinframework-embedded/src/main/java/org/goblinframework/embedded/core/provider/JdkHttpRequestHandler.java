000e239b21c4ddec54bd34a8ec35aeb95aabedfb
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-09 17:02:59 +0800

    org.goblinframework.embedded.core.provider.JdkHttpRequestHandler (TBC)

--- goblinframework-embedded/src/main/java/org/goblinframework/embedded/core/provider/JdkHttpRequestHandler.java
+++ goblinframework-embedded/src/main/java/org/goblinframework/embedded/core/provider/JdkHttpRequestHandler.java
@@ -4,13 +4,20 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.goblinframework.http.util.HttpUtils;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JdkHttpHandler implements HttpHandler {
+public class JdkHttpRequestHandler implements HttpHandler {
 
   @Override
   public void handle(HttpExchange exchange) throws IOException {
     String path = HttpUtils.compactContinuousSlashes(exchange.getRequestURI().getPath());
 
+    JdkHttpServletResponse response = new JdkHttpServletResponse(exchange);
+    if (HttpUtils.isMalformedPath(path)) {
+      response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Malformed request path specified");
+      response.close();
+      return;
+    }
   }
 }
