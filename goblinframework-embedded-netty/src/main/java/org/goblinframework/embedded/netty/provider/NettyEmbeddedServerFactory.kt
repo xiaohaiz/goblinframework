1880b934ce26010c6fff9e6940cae979ddaf4f84
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-10 21:21:11 +0800

    org.goblinframework.embedded.netty.provider.NettyEmbeddedServerFactory (TBC)

---
+++ goblinframework-embedded-netty/src/main/java/org/goblinframework/embedded/netty/provider/NettyEmbeddedServerFactory.kt
@@ -0,0 +1,17 @@
+package org.goblinframework.embedded.netty.provider
+
+import org.goblinframework.embedded.core.EmbeddedServer
+import org.goblinframework.embedded.core.EmbeddedServerMode
+import org.goblinframework.embedded.core.module.spi.EmbeddedServerFactory
+import org.goblinframework.embedded.core.setting.ServerSetting
+
+class NettyEmbeddedServerFactory : EmbeddedServerFactory {
+
+  override fun mode(): EmbeddedServerMode {
+    return EmbeddedServerMode.NETTY
+  }
+
+  override fun createEmbeddedServer(setting: ServerSetting): EmbeddedServer {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+}
