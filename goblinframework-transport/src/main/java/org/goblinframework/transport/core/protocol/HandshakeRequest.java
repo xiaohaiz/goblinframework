dbc4dc21473121cfa31d4276c4ad361ed156dc55
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-11 01:35:10 +0800

    org.goblinframework.transport.core.protocol.HandshakeRequest

---
+++ goblinframework-transport/src/main/java/org/goblinframework/transport/core/protocol/HandshakeRequest.java
@@ -0,0 +1,7 @@
+package org.goblinframework.transport.core.protocol;
+
+import java.io.Serializable;
+
+public class HandshakeRequest implements Serializable {
+  private static final long serialVersionUID = 2982940442592457140L;
+}
