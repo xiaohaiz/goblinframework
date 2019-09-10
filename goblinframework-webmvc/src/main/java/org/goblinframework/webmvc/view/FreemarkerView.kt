7e5ed27e5ebe4078adc191d4b3e45ed98faf0ec8
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-10 12:10:14 +0800

    org.goblinframework.webmvc.view.FreemarkerView

---
+++ goblinframework-webmvc/src/main/java/org/goblinframework/webmvc/view/FreemarkerView.kt
@@ -0,0 +1,24 @@
+package org.goblinframework.webmvc.view
+
+import freemarker.template.Template
+import org.goblinframework.webmvc.servlet.ServletRequest
+import org.goblinframework.webmvc.servlet.ServletResponse
+import org.springframework.http.MediaType
+import org.springframework.ui.Model
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils
+import org.springframework.validation.support.BindingAwareModelMap
+import java.nio.charset.Charset
+
+class FreemarkerView(private val template: Template,
+                     private val contentType: MediaType,
+                     private val encoding: String) : View {
+
+  override fun render(model: Model?, request: ServletRequest, response: ServletResponse) {
+    val m = model ?: BindingAwareModelMap()
+    val html = FreeMarkerTemplateUtils.processTemplateIntoString(template, m)
+    val bytes = html.toByteArray(Charset.forName(encoding))
+    response.headers.contentType = contentType
+    response.headers.contentLength = bytes.size.toLong()
+    response.body.write(bytes)
+  }
+}
