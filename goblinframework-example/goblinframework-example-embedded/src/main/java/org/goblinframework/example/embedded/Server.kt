e8f655d6a2f9fe1e4df2657eb1a68749262802d1
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-10 18:16:11 +0800

    org.goblinframework.example.embedded.controller.IndexController

--- goblinframework-example/goblinframework-example-embedded/src/main/java/org/goblinframework/example/embedded/Server.kt
+++ goblinframework-example/goblinframework-example-embedded/src/main/java/org/goblinframework/example/embedded/Server.kt
@@ -3,12 +3,45 @@ package org.goblinframework.example.embedded
 import org.goblinframework.bootstrap.core.StandaloneServer
 import org.goblinframework.core.container.SpringContainer
 import org.goblinframework.core.container.UseSpringContainer
+import org.goblinframework.embedded.core.EmbeddedServerMode
+import org.goblinframework.embedded.core.handler.DispatchServletHandler
+import org.goblinframework.embedded.core.manager.EmbeddedServerManager
+import org.goblinframework.embedded.core.resource.ClassPathStaticResourceManager
+import org.goblinframework.embedded.core.resource.MapStaticResourceBuffer
+import org.goblinframework.embedded.core.setting.ServerSetting
+import org.goblinframework.webmvc.handler.RequestHandlerManagerBuilder
+import org.goblinframework.webmvc.setting.RequestHandlerSetting
 
 @UseSpringContainer("/config/goblin-example-embedded.xml")
 class Server : StandaloneServer() {
 
   override fun doService(container: SpringContainer?) {
+    val ctx = container!!.applicationContext
+    val handler = DispatchServletHandler()
+    handler.registerWelcomeFile("/index.goblin")
 
+    val setting = RequestHandlerSetting.builder()
+        .name("GOBLIN")
+        .applyControllerSetting { it.applicationContext(ctx) }
+        .applyInterceptorSetting { it.applicationContext(ctx) }
+        .applyViewResolverSetting { it.applicationContext(ctx) }
+        .build()
+    val handlerManager = RequestHandlerManagerBuilder.INSTANCE.createRequestHandlerManager(setting)
+    handler.registerRequestHandlerManager(handlerManager, ".goblin")
+
+    val resourceBuffer = MapStaticResourceBuffer(4096)
+    val resourceManager = ClassPathStaticResourceManager(resourceBuffer, "/static", "/WEB-INF/static")
+    handler.registerStaticResourceManager(resourceManager)
+
+    val serverSetting = ServerSetting.builder()
+        .name("GOBLIN")
+        .mode(EmbeddedServerMode.JDK)
+        .applyHandlerSetting {
+          it.contextPath("/")
+          it.servletHandler(handler)
+        }
+        .build()
+    EmbeddedServerManager.INSTANCE.createServer(serverSetting).start()
   }
 
 }
