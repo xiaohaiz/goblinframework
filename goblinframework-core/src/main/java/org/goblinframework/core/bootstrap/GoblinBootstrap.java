086afbb912216ca443fe51e29b7cede92c37f9e6
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-10 21:12:29 +0800

    org.goblinframework.core.bootstrap.GoblinBootstrap

--- goblinframework-core/src/main/java/org/goblinframework/core/bootstrap/GoblinBootstrap.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/bootstrap/GoblinBootstrap.java
@@ -7,17 +7,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 final public class GoblinBootstrap {
   public static final Logger LOGGER = LoggerFactory.getLogger("goblin.core.Bootstrap");
 
+  private static final AtomicBoolean initialized = new AtomicBoolean();
+  private static final AtomicBoolean closed = new AtomicBoolean();
+
   public static void initialize() {
+    if (!initialized.compareAndSet(false, true)) {
+      return;
+    }
     EventBusBoss.INSTANCE.initialize();
     GoblinModuleManager.INSTANCE.executeInitialize().executeBootstrap();
     LOGGER.info("WELCOME");
   }
 
   public static void close() {
+    if (!closed.compareAndSet(false, true)) {
+      return;
+    }
     SpringContainerManager.INSTANCE.close();
     GoblinModuleManager.INSTANCE.executeFinalize();
     EventBusBoss.INSTANCE.close();
