494875140286a71c67045cbd90c95ed55d8beb95
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-11 14:21:28 +0800

    org.goblinframework.transport.server.setting.ServerSetting

--- goblinframework-core/src/main/java/org/goblinframework/core/util/SystemUtils.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/SystemUtils.java
@@ -1,9 +1,16 @@
 package org.goblinframework.core.util;
 
+import org.apache.commons.lang3.Validate;
+
 abstract public class SystemUtils extends org.apache.commons.lang3.SystemUtils {
 
   public static int estimateThreads() {
     int processors = Runtime.getRuntime().availableProcessors();
     return Math.max(processors, 2);
+  }
+
+  public static int estimateThreads(int threads) {
+    Validate.isTrue(threads >= 0);
+    return threads == 0 ? estimateThreads() : threads;
   }
 }
---
+++ goblinframework-transport-server/src/main/java/org/goblinframework/transport/server/setting/ServerSetting.java
@@ -0,0 +1,98 @@
+package org.goblinframework.transport.server.setting;
+
+import org.goblinframework.core.util.NetworkUtils;
+import org.goblinframework.core.util.SystemUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+final public class ServerSetting {
+
+  private final String name;
+  private final String host;
+  private final int port;
+  private final int bossThreads;
+  private final int workerThreads;
+
+  private ServerSetting(@NotNull ServerSettingBuilder builder) {
+    this.name = Objects.requireNonNull(builder.name);
+    this.host = Objects.requireNonNull(builder.host);
+    this.port = builder.port;
+    this.bossThreads = builder.bossThreads;
+    this.workerThreads = builder.workerThreads;
+  }
+
+  @NotNull
+  public String name() {
+    return name;
+  }
+
+  @NotNull
+  public String host() {
+    return host;
+  }
+
+  public int port() {
+    return port;
+  }
+
+  public int bossThreads() {
+    return bossThreads;
+  }
+
+  public int workerThreads() {
+    return workerThreads;
+  }
+
+  @NotNull
+  public static ServerSettingBuilder builder() {
+    return new ServerSettingBuilder();
+  }
+
+  final public static class ServerSettingBuilder {
+
+    private String name;
+    private String host = NetworkUtils.ALL_HOST;
+    private int port = NetworkUtils.RANDOM_PORT;
+    private int bossThreads = 1;
+    private int workerThreads = SystemUtils.estimateThreads();
+
+    private ServerSettingBuilder() {
+    }
+
+    @NotNull
+    public ServerSettingBuilder name(@NotNull String name) {
+      this.name = name;
+      return this;
+    }
+
+    @NotNull
+    public ServerSettingBuilder setHost(@NotNull String host) {
+      this.host = host;
+      return this;
+    }
+
+    @NotNull
+    public ServerSettingBuilder setPort(int port) {
+      this.port = port;
+      return this;
+    }
+
+    @NotNull
+    public ServerSettingBuilder bossThreads(int bossThreads) {
+      this.bossThreads = SystemUtils.estimateThreads(bossThreads);
+      return this;
+    }
+
+    @NotNull
+    public ServerSettingBuilder workerThreads(int workerThreads) {
+      this.workerThreads = SystemUtils.estimateThreads(workerThreads);
+      return this;
+    }
+
+    @NotNull
+    public ServerSetting build() {
+      return new ServerSetting(this);
+    }
+  }
+}
