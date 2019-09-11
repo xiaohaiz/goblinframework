6fe3389f0086a8096fe28f73fce85de9cb3ebb9b
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-11 20:50:11 +0800

    org.goblinframework.transport.server.manager.TransportServer

--- goblinframework-transport-server/src/main/java/org/goblinframework/transport/server/manager/TransportServer.kt
+++ goblinframework-transport-server/src/main/java/org/goblinframework/transport/server/manager/TransportServer.kt
@@ -3,25 +3,40 @@ package org.goblinframework.transport.server.manager
 import org.goblinframework.api.common.Lifecycle
 import org.goblinframework.core.mbean.GoblinManagedBean
 import org.goblinframework.core.mbean.GoblinManagedObject
+import org.goblinframework.core.util.StopWatch
 import org.goblinframework.transport.server.setting.ServerSetting
+import java.util.concurrent.atomic.AtomicReference
 
 @GoblinManagedBean("TRANSPORT.SERVER")
-class TransportServer(private val setting: ServerSetting)
+class TransportServer
+internal constructor(private val setting: ServerSetting)
   : GoblinManagedObject(), Lifecycle, TransportServerMXBean {
 
+  private val watch = StopWatch()
+  private val server = AtomicReference<TransportServerImpl>()
+
+  @Synchronized
   override fun start() {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    if (server.get() == null) {
+      server.set(TransportServerImpl(setting))
+    }
   }
 
   override fun stop() {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    server.getAndSet(null)?.close()
   }
 
   override fun isRunning(): Boolean {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    return server.get() != null
   }
 
   internal fun close() {
     unregisterIfNecessary()
+    watch.stop()
+    stop()
+  }
+
+  override fun getUpTime(): String {
+    return watch.toString()
   }
 }
--- goblinframework-transport-server/src/main/java/org/goblinframework/transport/server/manager/TransportServerMXBean.kt
+++ goblinframework-transport-server/src/main/java/org/goblinframework/transport/server/manager/TransportServerMXBean.kt
@@ -2,4 +2,8 @@ package org.goblinframework.transport.server.manager
 
 import java.lang.management.PlatformManagedObject
 
-interface TransportServerMXBean : PlatformManagedObject
+interface TransportServerMXBean : PlatformManagedObject {
+
+  fun getUpTime(): String
+
+}
