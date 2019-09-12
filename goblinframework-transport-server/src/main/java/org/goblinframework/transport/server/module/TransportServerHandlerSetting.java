883dca7b28254b9411df3c18d694ac2d9af0dbda
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-12 17:33:52 +0800

    org.goblinframework.transport.server.module.TransportServerHandlerSetting

---
+++ goblinframework-transport-server/src/main/java/org/goblinframework/transport/server/module/TransportServerHandlerSetting.java
@@ -0,0 +1,45 @@
+package org.goblinframework.transport.server.module;
+
+import org.goblinframework.transport.server.handler.DefaultHandshakeRequestHandler;
+import org.goblinframework.transport.server.handler.HandshakeRequestHandler;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+final public class TransportServerHandlerSetting {
+
+  private final HandshakeRequestHandler handshakeRequestHandler;
+
+  private TransportServerHandlerSetting(@NotNull TransportServerHandlerSettingBuilder builder) {
+    this.handshakeRequestHandler = Objects.requireNonNull(builder.handshakeRequestHandler);
+  }
+
+  @NotNull
+  public HandshakeRequestHandler handshakeRequestHandler() {
+    return handshakeRequestHandler;
+  }
+
+  @NotNull
+  public static TransportServerHandlerSettingBuilder builder() {
+    return new TransportServerHandlerSettingBuilder();
+  }
+
+  public static class TransportServerHandlerSettingBuilder {
+
+    private HandshakeRequestHandler handshakeRequestHandler = DefaultHandshakeRequestHandler.INSTANCE;
+
+    private TransportServerHandlerSettingBuilder() {
+    }
+
+    @NotNull
+    public TransportServerHandlerSettingBuilder handshakeRequestHandler(@NotNull HandshakeRequestHandler handshakeRequestHandler) {
+      this.handshakeRequestHandler = handshakeRequestHandler;
+      return this;
+    }
+
+    @NotNull
+    public TransportServerHandlerSetting build() {
+      return new TransportServerHandlerSetting(this);
+    }
+  }
+}
