fe84a13decd8c229632b407fc14cdb62e9ab82c8
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-13 22:42:47 +0800

    org.goblinframework.core.transcoder.TranscoderTest

--- goblinframework-core/src/main/java/org/goblinframework/core/transcoder/DecodedObject.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/transcoder/DecodedObject.java
@@ -2,14 +2,14 @@ package org.goblinframework.core.transcoder;
 
 public class DecodedObject {
 
-  public Object object;
+  public Object decoded;
   public byte serializer;
 
   public DecodedObject() {
   }
 
-  public DecodedObject(Object object, byte serializer) {
-    this.object = object;
+  public DecodedObject(Object decoded, byte serializer) {
+    this.decoded = decoded;
     this.serializer = serializer;
   }
 }
---
+++ goblinframework-core/src/test/java/org/goblinframework/core/transcoder/TranscoderTest.kt
@@ -0,0 +1,26 @@
+package org.goblinframework.core.transcoder
+
+import org.bson.types.ObjectId
+import org.goblinframework.core.serialization.SerializerManager
+import org.goblinframework.core.serialization.SerializerMode
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+class TranscoderTest {
+
+  @Test
+  fun transcode() {
+    val serializer = SerializerManager.INSTANCE.getSerializer(SerializerMode.FST)
+    val source = ObjectId()
+    val bos = ByteArrayOutputStream()
+    Transcoder.encode(bos, source, serializer)
+    val bis = ByteArrayInputStream(bos.toByteArray())
+    bos.close()
+    val obj = Transcoder.decode(bis)
+    assertEquals(serializer.mode().id, obj.serializer)
+    assertEquals(source, obj.decoded)
+    bis.close()
+  }
+}
