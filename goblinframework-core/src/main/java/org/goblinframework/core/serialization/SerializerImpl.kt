8add75f6d7ed7760f5110382f83b70ba7e70fb3c
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-13 16:07:09 +0800

    org.goblinframework.core.serialization.SerializerMXBean

--- goblinframework-core/src/main/java/org/goblinframework/core/serialization/SerializerImpl.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/serialization/SerializerImpl.kt
@@ -1,40 +1,98 @@
 package org.goblinframework.core.serialization
 
+import org.goblinframework.core.exception.GoblinSerializationException
 import org.goblinframework.core.mbean.GoblinManagedBean
 import org.goblinframework.core.mbean.GoblinManagedObject
+import org.goblinframework.core.util.StopWatch
 
 import java.io.InputStream
 import java.io.OutputStream
+import java.util.concurrent.atomic.LongAdder
 
 @GoblinManagedBean(type = "CORE", name = "Serializer")
 internal class SerializerImpl(private val serializer: Serializer)
   : GoblinManagedObject(), Serializer, SerializerMXBean {
+
+  private val watch = StopWatch()
+  private val serializeCount = LongAdder()
+  private val serializeExceptionCount = LongAdder()
+  private val deserializeCount = LongAdder()
+  private val deserializeExceptionCount = LongAdder()
 
   override fun mode(): SerializerMode {
     return serializer.mode()
   }
 
   override fun serialize(obj: Any, outStream: OutputStream) {
-    serializer.serialize(obj, outStream)
+    try {
+      serializer.serialize(obj, outStream)
+    } catch (ex: GoblinSerializationException) {
+      serializeExceptionCount.increment()
+      throw ex
+    } finally {
+      serializeCount.increment()
+    }
   }
 
   override fun serialize(obj: Any): ByteArray {
-    return serializer.serialize(obj)
+    try {
+      return serializer.serialize(obj)
+    } catch (ex: GoblinSerializationException) {
+      serializeExceptionCount.increment()
+      throw ex
+    } finally {
+      serializeCount.increment()
+    }
   }
 
   override fun deserialize(inStream: InputStream): Any {
-    return serializer.deserialize(inStream)
+    try {
+      return serializer.deserialize(inStream)
+    } catch (ex: GoblinSerializationException) {
+      deserializeExceptionCount.increment()
+      throw ex
+    } finally {
+      deserializeCount.increment()
+    }
   }
 
   override fun deserialize(bs: ByteArray): Any {
-    return serializer.deserialize(bs)
+    try {
+      return serializer.deserialize(bs)
+    } catch (ex: GoblinSerializationException) {
+      deserializeExceptionCount.increment()
+      throw ex
+    } finally {
+      deserializeCount.increment()
+    }
+  }
+
+  override fun getUpTime(): String {
+    return watch.toString()
   }
 
   override fun getMode(): SerializerMode {
     return mode()
   }
 
+  override fun getSerializeCount(): Long {
+    return serializeCount.sum()
+  }
+
+  override fun getSerializeExceptionCount(): Long {
+    return serializeExceptionCount.sum()
+  }
+
+  override fun getDeserializeCount(): Long {
+    return deserializeCount.sum()
+  }
+
+  override fun getDeserializeExceptionCount(): Long {
+    return deserializeExceptionCount.sum()
+  }
+
   fun close() {
     unregisterIfNecessary()
+    watch.stop()
   }
 }
