d08dde25d75dc471193c83c46e643f92475a96a6
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-13 09:28:21 +0800

    transport.protocol

--- goblinframework-transport/src/main/java/org/goblinframework/transport/core/protocol/TransportProtocol.java
+++ goblinframework-transport/src/main/java/org/goblinframework/transport/core/protocol/TransportProtocol.java
@@ -1,5 +1,15 @@
 package org.goblinframework.transport.core.protocol;
 
+import org.goblinframework.api.common.GoblinException;
+import org.goblinframework.core.util.ClassUtils;
+import org.goblinframework.transport.core.codec.ClassResolver;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
+import java.util.IdentityHashMap;
+import java.util.Properties;
+
 /**
  * <pre>
  * +----------+-----------+----------+
@@ -15,6 +25,29 @@ abstract public class TransportProtocol {
   private static final byte SERIALIZER_ID_MASK = (byte) 0x3f; // 0011 1111
 
   public static final byte DEFAULT_SERIALIZER_ID = 1;
+
+  private static final IdentityHashMap<Class<?>, Byte> serializerIds = new IdentityHashMap<>();
+
+  static {
+    Properties props = new Properties();
+    ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
+    ClassPathResource resource = new ClassPathResource("META-INF/goblin/transport.protocol", classLoader);
+    try (InputStream inStream = resource.getInputStream()) {
+      props.load(inStream);
+      for (String name : props.stringPropertyNames()) {
+        String value = props.getProperty(name);
+        Class<?> clazz = ClassResolver.INSTANCE.resolve(name);
+        byte serializerId = Byte.parseByte(value);
+        serializerIds.put(clazz, serializerId);
+      }
+    } catch (Exception ex) {
+      throw new GoblinException(ex);
+    }
+  }
+
+  public static byte getSerializerId(@NotNull Class<?> clazz) {
+    return serializerIds.getOrDefault(clazz, (byte) 0);
+  }
 
   public static boolean isPayloadEnabled(byte header) {
     return (header & PAYLOAD_FLAG) != 0;
