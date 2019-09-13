3fe1e02a6c0aeeaf83508ae520c1c9ce393c91d5
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-13 22:45:55 +0800

    More log messages.

--- goblinframework-core/src/test/java/org/goblinframework/core/serialization/SerializerTest.kt
+++ goblinframework-core/src/test/java/org/goblinframework/core/serialization/SerializerTest.kt
@@ -3,9 +3,12 @@ package org.goblinframework.core.serialization
 import org.bson.types.ObjectId
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.slf4j.LoggerFactory
 import java.time.Instant
 
 abstract class SerializerTest {
+
+  private val logger = LoggerFactory.getLogger(javaClass)
 
   abstract fun serializer(): Serializer
 
@@ -14,6 +17,7 @@ abstract class SerializerTest {
     val serializer = serializer()
     val before = ObjectId()
     val bs = serializer.serialize(before)
+    logger.debug("${ObjectId::class.java.name} serialized: ${bs.size}")
     val after = serializer.deserialize(bs) as ObjectId
     assertEquals(before, after)
   }
@@ -23,6 +27,7 @@ abstract class SerializerTest {
     val serializer = serializer()
     val before = Instant.now()
     val bs = serializer.serialize(before)
+    logger.debug("${Instant::class.java.name} serialized: ${bs.size}")
     val after = serializer.deserialize(bs) as Instant
     assertEquals(before.toEpochMilli(), after.toEpochMilli())
   }
