f65b77702239ca7e78a547438afe70a779f50204
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-13 14:18:10 +0800

    org.goblinframework.transport.core.protocol.TransportResponseException

---
+++ goblinframework-transport/src/main/java/org/goblinframework/transport/core/protocol/TransportResponseException.java
@@ -0,0 +1,34 @@
+package org.goblinframework.transport.core.protocol;
+
+import org.goblinframework.api.common.GoblinException;
+
+import java.util.Objects;
+
+public class TransportResponseException extends GoblinException {
+  private static final long serialVersionUID = -4818779160598751000L;
+
+  private final TransportResponseCode code;
+
+  public TransportResponseException(TransportResponseCode code) {
+    this.code = Objects.requireNonNull(code);
+  }
+
+  public TransportResponseException(TransportResponseCode code, String message) {
+    super(message);
+    this.code = Objects.requireNonNull(code);
+  }
+
+  public TransportResponseException(TransportResponseCode code, String message, Throwable cause) {
+    super(message, cause);
+    this.code = Objects.requireNonNull(code);
+  }
+
+  public TransportResponseException(TransportResponseCode code, Throwable cause) {
+    super(cause);
+    this.code = Objects.requireNonNull(code);
+  }
+
+  public TransportResponseCode getCode() {
+    return code;
+  }
+}
