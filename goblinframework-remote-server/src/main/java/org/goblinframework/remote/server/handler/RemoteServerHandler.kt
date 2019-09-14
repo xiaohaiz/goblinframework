d9ed4ff312f4c3594f8af6448d0bcca575194e64
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-14 15:23:19 +0800

    org.goblinframework.remote.server.handler.RemoteServerHandler

---
+++ goblinframework-remote-server/src/main/java/org/goblinframework/remote/server/handler/RemoteServerHandler.kt
@@ -0,0 +1,17 @@
+package org.goblinframework.remote.server.handler
+
+import org.goblinframework.api.annotation.Singleton
+import org.goblinframework.transport.server.handler.TransportRequestContext
+import org.goblinframework.transport.server.handler.TransportRequestHandler
+
+@Singleton
+class RemoteServerHandler private constructor() : TransportRequestHandler {
+
+  companion object {
+    @JvmField val INSTANCE = RemoteServerHandler()
+  }
+
+  override fun handleTransportRequest(ctx: TransportRequestContext) {
+
+  }
+}
--- goblinframework-remote-server/src/main/java/org/goblinframework/remote/server/handler/RemoteServerImpl.kt
+++ goblinframework-remote-server/src/main/java/org/goblinframework/remote/server/handler/RemoteServerImpl.kt
@@ -1,7 +1,27 @@
 package org.goblinframework.remote.server.handler
 
+import org.goblinframework.transport.server.channel.TransportServerManager
+import org.goblinframework.transport.server.setting.TransportServerSetting
+
 class RemoteServerImpl internal constructor() {
 
+  companion object {
+    private const val SERVER_NAME = "goblin.remote.Server"
+  }
+
+  init {
+    val setting = TransportServerSetting.builder()
+        .name(SERVER_NAME)
+        .enableDebugMode()
+        .applyHandlerSetting {
+          it.transportRequestHandler(RemoteServerHandler.INSTANCE)
+        }
+        .build()
+    val server = TransportServerManager.INSTANCE.createTransportServer(setting)
+    server.start()
+  }
+
   internal fun close() {
+    TransportServerManager.INSTANCE.closeTransportServer(SERVER_NAME)
   }
 }
