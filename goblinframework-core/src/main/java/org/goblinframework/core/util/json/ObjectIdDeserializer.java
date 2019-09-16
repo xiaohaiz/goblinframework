1e768e9263dff8a057811f4ae506da365575d46d
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-16 11:16:54 +0800

    org.goblinframework.core.util.json.ObjectIdDeserializer

--- goblinframework-core/src/main/java/org/goblinframework/core/util/json/ObjectIdDeserializer.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/json/ObjectIdDeserializer.java
@@ -3,19 +3,23 @@ package org.goblinframework.core.util.json;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
 import org.bson.types.ObjectId;
 
 import java.io.IOException;
 
-public class ObjectIdDeserializer extends JsonDeserializer<ObjectId> {
+public class ObjectIdDeserializer extends StdScalarDeserializer<ObjectId> {
+
+  public ObjectIdDeserializer() {
+    super(ObjectId.class);
+  }
 
   @Override
   public ObjectId deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-    String s = p.getText();
-    if (!ObjectId.isValid(s)) {
+    String value = p.getValueAsString();
+    if (!ObjectId.isValid(value)) {
       return null;
     }
-    return new ObjectId(s);
+    return new ObjectId(value);
   }
 }
