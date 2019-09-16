71dd3a4d78b13c9ef1b928b56f40b1405eedf753
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-16 19:53:35 +0800

    org.goblinframework.registry.zookeeper.module.config.ZookeeperClientConfig

---
+++ goblinframework-registry-zookeeper/src/main/java/org/goblinframework/registry/zookeeper/module/config/ZookeeperClientConfig.java
@@ -0,0 +1,7 @@
+package org.goblinframework.registry.zookeeper.module.config;
+
+import java.io.Serializable;
+
+public class ZookeeperClientConfig implements Serializable {
+  private static final long serialVersionUID = 3544314462066772928L;
+}
