b1c7e5ffa6e3df4a9f24914c8c64cfe4df820fe2
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-16 19:36:27 +0800

    org.goblinframework.core.config.ConfigMappingLoader

--- goblinframework-core/src/main/java/org/goblinframework/core/config/ConfigMappingLoader.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/config/ConfigMappingLoader.kt
@@ -1,19 +1,28 @@
 package org.goblinframework.core.config
 
+import org.goblinframework.core.mapper.JsonMapper
 import org.goblinframework.core.mbean.GoblinManagedBean
 import org.goblinframework.core.mbean.GoblinManagedObject
-import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicReference
 
 @GoblinManagedBean("CORE")
 class ConfigMappingLoader internal constructor() : GoblinManagedObject(), ConfigMappingLoaderMXBean {
 
-  private val initialized = AtomicBoolean()
+  private val mapping = AtomicReference<ConfigMapping>()
+
+  init {
+    mapping.set(ConfigMapping())
+  }
 
   internal fun initialize(scanner: ConfigLocationScanner) {
     if (scanner.getConfigPathUrl() == null) {
       return
     }
-    initialized.set(true)
+    val resource = scanner.scan("goblin.json").firstOrNull() ?: return
+    val mapping = resource.inputStream.use {
+      JsonMapper.asObject(it, ConfigMapping::class.java)
+    }
+    this.mapping.set(mapping)
   }
 
   internal fun close() {
