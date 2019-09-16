339ba01d0817abdff4932eae3257a39bb8309fe7
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-16 15:53:25 +0800

    org.goblinframework.core.config.ConfigLocationScanner

---
+++ goblinframework-core/src/main/java/org/goblinframework/core/config/ConfigLocationScanner.kt
@@ -0,0 +1,61 @@
+package org.goblinframework.core.config
+
+import org.goblinframework.api.annotation.Singleton
+import org.goblinframework.core.mbean.GoblinManagedBean
+import org.goblinframework.core.mbean.GoblinManagedObject
+import org.goblinframework.core.util.ClassUtils
+import org.goblinframework.core.util.StringUtils
+import java.io.File
+import java.net.URL
+import java.util.concurrent.atomic.AtomicBoolean
+
+
+@Singleton
+@GoblinManagedBean("CORE")
+class ConfigLocationScanner private constructor() : GoblinManagedObject(), ConfigLocationScannerMXBean {
+
+  companion object {
+    val INSTANCE = ConfigLocationScanner()
+  }
+
+  private val foundInFilesystem = AtomicBoolean()
+  private var configPathUrl: URL? = null
+  private val candidatePaths = mutableListOf<File>()
+
+  init {
+    initialize()
+  }
+
+  private fun initialize() {
+    val configPath = "config/goblin.ini"
+    val classLoader = ClassUtils.getDefaultClassLoader()
+    val url = classLoader.getResource(configPath)
+    if (url == null) {
+      logger.warn("No config [$configPath] found in classpath, ignore loading config(s)")
+      return
+    }
+    configPathUrl = url
+
+    var foundInFileSystem = false
+    var file: String
+    when {
+      StringUtils.equals("jar", url.protocol) -> {
+        file = url.file
+        file = StringUtils.substringAfter(file, "file:");
+        file = StringUtils.substringBeforeLast(file, "!");
+      }
+      StringUtils.equals("file", url.protocol) -> {
+        foundInFileSystem = true
+        file = url.file
+      }
+      else -> throw UnsupportedOperationException()
+    }
+    this.foundInFilesystem.set(foundInFileSystem)
+
+    var parent: File = File(file).parentFile
+    while (true) {
+      candidatePaths.add(parent)
+      parent = parent.parentFile ?: break
+    }
+  }
+}
--- goblinframework-core/src/main/java/org/goblinframework/core/mbean/GoblinManagedObject.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/mbean/GoblinManagedObject.java
@@ -3,12 +3,16 @@ package org.goblinframework.core.mbean;
 import org.goblinframework.core.util.AnnotationUtils;
 import org.goblinframework.core.util.ClassUtils;
 import org.goblinframework.core.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.management.ObjectName;
 import java.lang.management.PlatformManagedObject;
 import java.util.function.Supplier;
 
 abstract public class GoblinManagedObject implements PlatformManagedObject {
+
+  protected final Logger logger = LoggerFactory.getLogger(getClass());
 
   private final ObjectName objectName;
   private final boolean registerMBean;
