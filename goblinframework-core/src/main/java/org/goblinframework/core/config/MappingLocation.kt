bb9569761aba5ad16786be3f4b1607412cab181b
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-17 00:01:21 +0800

    org.goblinframework.core.config.MappingLocationScanner

---
+++ goblinframework-core/src/main/java/org/goblinframework/core/config/MappingLocation.kt
@@ -0,0 +1,5 @@
+package org.goblinframework.core.config
+
+import org.springframework.core.io.Resource
+
+class MappingLocation(val resource: Resource)
---
+++ goblinframework-core/src/main/java/org/goblinframework/core/config/MappingLocationScanner.kt
@@ -0,0 +1,53 @@
+package org.goblinframework.core.config
+
+import org.goblinframework.core.mbean.GoblinManagedBean
+import org.goblinframework.core.mbean.GoblinManagedObject
+import org.goblinframework.core.util.ClassUtils
+import org.goblinframework.core.util.IOUtils
+import org.springframework.core.io.ByteArrayResource
+import java.util.concurrent.atomic.AtomicReference
+
+@GoblinManagedBean("CORE")
+class MappingLocationScanner
+internal constructor(configLocationScanner: ConfigLocationScanner)
+  : GoblinManagedObject(), MappingLocationScannerMXBean {
+
+  companion object {
+    private val SCAN_SEQUENCE = listOf(
+        "config/goblin-test.json",
+        "META-INF/goblin/goblin-test.json",
+        "config/goblin.json",
+        "META-INF/goblin/goblin.json")
+  }
+
+  private val mappingLocation = AtomicReference<MappingLocation>()
+
+  init {
+    val classLoader = ClassUtils.getDefaultClassLoader()
+    for (path in SCAN_SEQUENCE) {
+      val url = classLoader.getResource(path)
+      if (url != null) {
+        val resource = url.openStream().use {
+          val bs = IOUtils.toByteArray(it)
+          ByteArrayResource(bs)
+        }
+        mappingLocation.set(MappingLocation(resource))
+        break
+      }
+    }
+    if (mappingLocation.get() == null) {
+      val resource = configLocationScanner.scan("goblin.json").firstOrNull()
+      resource?.run {
+        mappingLocation.set(MappingLocation(this))
+      }
+    }
+  }
+
+  fun getMappingLocation(): MappingLocation? {
+    return mappingLocation.get()
+  }
+
+  internal fun close() {
+    unregisterIfNecessary()
+  }
+}
