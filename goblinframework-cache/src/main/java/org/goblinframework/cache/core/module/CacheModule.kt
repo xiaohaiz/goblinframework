4fe3b5fba2c1c69bd78c5017b3fe7470e455fcc4
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-17 17:02:41 +0800

    org.goblinframework.cache.core.module.CacheModule

---
+++ goblinframework-cache/src/main/java/org/goblinframework/cache/core/module/CacheModule.kt
@@ -0,0 +1,36 @@
+package org.goblinframework.cache.core.module
+
+import org.goblinframework.api.annotation.Install
+import org.goblinframework.core.bootstrap.GoblinModule
+import org.goblinframework.core.bootstrap.GoblinModuleBootstrapContext
+import org.goblinframework.core.bootstrap.GoblinModuleFinalizeContext
+import org.goblinframework.core.bootstrap.GoblinModuleInitializeContext
+
+@Install
+class CacheModule : GoblinModule {
+
+  override fun name(): String {
+    return "CACHE"
+  }
+
+  override fun initialize(ctx: GoblinModuleInitializeContext) {
+    ctx.createChildModuleManager()
+        .module("CACHE:COUCHBASE")
+        .module("CACHE:REDIS")
+        .initialize(ctx)
+  }
+
+  override fun bootstrap(ctx: GoblinModuleBootstrapContext) {
+    ctx.createChildModuleManager()
+        .module("CACHE:COUCHBASE")
+        .module("CACHE:REDIS")
+        .bootstrap(ctx)
+  }
+
+  override fun finalize(ctx: GoblinModuleFinalizeContext) {
+    ctx.createChildModuleManager()
+        .module("CACHE:COUCHBASE")
+        .module("CACHE:REDIS")
+        .finalize(ctx)
+  }
+}
