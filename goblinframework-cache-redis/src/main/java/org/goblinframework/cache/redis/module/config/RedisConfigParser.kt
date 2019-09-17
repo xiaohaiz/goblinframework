0a6bde504beb89c9d280b475a2720535ffaba383
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-17 17:26:26 +0800

    org.goblinframework.cache.redis.module.config.RedisConfigParser

---
+++ goblinframework-cache-redis/src/main/java/org/goblinframework/cache/redis/module/config/RedisConfigParser.kt
@@ -0,0 +1,33 @@
+package org.goblinframework.cache.redis.module.config
+
+import org.goblinframework.core.config.BufferedConfigParser
+import org.goblinframework.core.config.ConfigLoader
+import org.goblinframework.core.exception.GoblinConfigException
+import org.goblinframework.core.util.StringUtils
+
+class RedisConfigParser internal constructor() : BufferedConfigParser<RedisConfig>() {
+
+  companion object {
+    private const val DEFAULT_REDIS_PORT = 6379
+  }
+
+  override fun initialize() {
+    val mapping = ConfigLoader.INSTANCE.getMapping()
+    parseToMap(mapping, "redis", RedisConfigMapper::class.java)
+        .map { it.value.also { c -> c.name = it.key } }
+        .map { RedisConfig(it) }
+        .forEach { putIntoBuffer(it.getName(), it) }
+  }
+
+  override fun doProcessConfig(config: RedisConfig) {
+    val mapper = config.mapper
+    if (StringUtils.isBlank(mapper.servers)) {
+      throw GoblinConfigException("RedisConfig.servers is required")
+    }
+    if (mapper.mode == null) {
+      throw GoblinConfigException("RedisConfig.mode is required")
+    }
+    val servers = StringUtils.formalizeServers(mapper.servers, " ") { DEFAULT_REDIS_PORT }
+    mapper.servers = servers
+  }
+}
--- goblinframework-core/src/main/java/org/goblinframework/core/util/StringUtils.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/StringUtils.java
@@ -3,7 +3,10 @@ package org.goblinframework.core.util;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 abstract public class StringUtils extends org.apache.commons.lang3.StringUtils {
 
@@ -17,5 +20,37 @@ abstract public class StringUtils extends org.apache.commons.lang3.StringUtils {
   public static String defaultString(@Nullable final String str,
                                      @NotNull final Supplier<String> defaultStrSupplier) {
     return str == null ? defaultStrSupplier.get() : str;
+  }
+
+  @NotNull
+  public static String formalizeServers(@Nullable final String servers,
+                                        @Nullable final String separator,
+                                        @Nullable final Supplier<Integer> portSupplier) {
+    if (isBlank(servers)) {
+      return EMPTY;
+    }
+    String s = replace(servers, ",", " ");
+    s = replace(s, ";", " ");
+    List<String> formalized = Arrays.stream(split(s, " "))
+        .filter(StringUtils::isNotBlank)
+        .map(StringUtils::trim)
+        .map(e -> {
+          if (portSupplier == null) {
+            return e;
+          }
+          Integer port = portSupplier.get();
+          if (port == null) {
+            return e;
+          }
+          if (contains(e, ":")) {
+            return e;
+          } else {
+            return e + ":" + port;
+          }
+        })
+        .distinct()
+        .sorted()
+        .collect(Collectors.toList());
+    return join(formalized, (separator == null ? " " : separator));
   }
 }
