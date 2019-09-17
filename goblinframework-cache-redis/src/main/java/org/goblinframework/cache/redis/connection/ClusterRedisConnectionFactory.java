936a12948affcb97b13700c4e78fb0a23ee4fb0a
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-17 21:48:35 +0800

    org.goblinframework.cache.redis.connection.ClusterRedisConnectionFactory

---
+++ goblinframework-cache-redis/src/main/java/org/goblinframework/cache/redis/connection/ClusterRedisConnectionFactory.java
@@ -0,0 +1,38 @@
+package org.goblinframework.cache.redis.connection;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import org.apache.commons.pool2.BasePooledObjectFactory;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.goblinframework.cache.redis.transcoder.RedisTranscoder;
+import org.jetbrains.annotations.NotNull;
+
+public class ClusterRedisConnectionFactory extends BasePooledObjectFactory<RedisConnection> {
+
+  private final RedisClient redisClient;
+  private final RedisTranscoder redisTranscoder;
+
+  public ClusterRedisConnectionFactory(@NotNull RedisClient redisClient,
+                                       @NotNull RedisTranscoder redisTranscoder) {
+    this.redisClient = redisClient;
+    this.redisTranscoder = redisTranscoder;
+  }
+
+  @Override
+  public RedisConnection create() {
+    return createClusterRedisConnection(redisClient, redisTranscoder);
+  }
+
+  @Override
+  public PooledObject<RedisConnection> wrap(RedisConnection obj) {
+    return new DefaultPooledObject<>(obj);
+  }
+
+  @NotNull
+  public static ClusterRedisConnection createClusterRedisConnection(@NotNull RedisClient redisClient,
+                                                                    @NotNull RedisTranscoder redisTranscoder) {
+    StatefulRedisConnection<String, Object> connection = redisClient.connect(redisTranscoder);
+    return new ClusterRedisConnection(connection);
+  }
+}
