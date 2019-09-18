46279c5e8764d3a79cdc1ad4974e000e5f33fd35
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-18 17:52:41 +0800

    org.goblinframework.dao.mysql.client.MysqlClient

---
+++ goblinframework-dao-mysql/src/main/java/org/goblinframework/dao/mysql/client/MysqlClient.java
@@ -0,0 +1,38 @@
+package org.goblinframework.dao.mysql.client;
+
+import org.goblinframework.core.mbean.GoblinManagedBean;
+import org.goblinframework.core.mbean.GoblinManagedObject;
+import org.goblinframework.dao.mysql.module.config.DataSourceConfig;
+import org.goblinframework.dao.mysql.module.config.MysqlConfig;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+@GoblinManagedBean(type = "DAO.MYSQL")
+final public class MysqlClient extends GoblinManagedObject implements MysqlClientMXBean {
+
+  private final DataSource master;
+  private final List<DataSource> slaves = new LinkedList<>();
+
+  public MysqlClient(@NotNull MysqlConfig config) {
+    this.master = DataSourceBuilder.buildDataSource((DataSourceConfig) config.getMaster());
+    Arrays.stream(config.getSlaveList())
+        .map(e -> (DataSourceConfig) e)
+        .map(DataSourceBuilder::buildDataSource)
+        .forEach(slaves::add);
+  }
+
+  void destroy() {
+    unregisterIfNecessary();
+    master.destroy();
+    slaves.forEach(DataSource::destroy);
+  }
+
+  @NotNull
+  @Override
+  public DataSourceMXBean getMaster() {
+    return master;
+  }
+}
