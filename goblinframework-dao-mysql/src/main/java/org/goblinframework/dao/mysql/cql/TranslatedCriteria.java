918eb5fb470b90509c00d8022cf943c6074d2ebd
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-19 17:32:59 +0800

    org.goblinframework.dao.mysql.cql.TranslatedCriteria

---
+++ goblinframework-dao-mysql/src/main/java/org/goblinframework/dao/mysql/cql/TranslatedCriteria.java
@@ -0,0 +1,20 @@
+package org.goblinframework.dao.mysql.cql;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+
+/**
+ * Translated criteria data structure.
+ */
+public class TranslatedCriteria {
+
+  public String sql;
+  public MapSqlParameterSource parameterSource;
+
+  public TranslatedCriteria() {
+  }
+
+  public TranslatedCriteria(String sql, MapSqlParameterSource parameterSource) {
+    this.sql = sql;
+    this.parameterSource = parameterSource;
+  }
+}
