047a33fe5e90da201621bae2c6d289b316010c5d
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-19 17:32:17 +0800

    org.goblinframework.dao.mysql.cql.NamedParameterSQL

---
+++ goblinframework-dao-mysql/src/main/java/org/goblinframework/dao/mysql/cql/NamedParameterSQL.java
@@ -0,0 +1,20 @@
+package org.goblinframework.dao.mysql.cql;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+
+/**
+ * SQL and named parameters.
+ */
+public class NamedParameterSQL {
+
+  public String sql;
+  public MapSqlParameterSource source;
+
+  public NamedParameterSQL() {
+  }
+
+  public NamedParameterSQL(String sql, MapSqlParameterSource source) {
+    this.sql = sql;
+    this.source = source;
+  }
+}
