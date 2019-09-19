50ee6e21748a2a175bdc5becd12d70222006bf15
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-19 11:42:01 +0800

    org.goblinframework.cache.core.provider.NoOpCache

---
+++ goblinframework-cache/src/main/java/org/goblinframework/cache/core/provider/NoOpCacheBuilder.kt
@@ -0,0 +1,22 @@
+package org.goblinframework.cache.core.provider
+
+import org.goblinframework.api.annotation.Install
+import org.goblinframework.cache.core.cache.CacheSystem
+import org.goblinframework.cache.core.cache.GoblinCache
+import org.goblinframework.cache.core.cache.GoblinCacheBuilder
+
+@Install
+class NoOpCacheBuilder : GoblinCacheBuilder {
+
+  override fun decorateCacheName(name: String): String {
+    return "NOP"
+  }
+
+  override fun getCacheSystem(): CacheSystem {
+    return CacheSystem.NOP
+  }
+
+  override fun getCache(name: String): GoblinCache? {
+    return NoOpCache.INSTANCE
+  }
+}
