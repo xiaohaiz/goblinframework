fba5e1fc61c6bf196d9b252336428b8257776316
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-19 23:08:21 +0800

    org.goblinframework.dao.mysql.persistence.GoblinStaticPersistenceTest

---
+++ goblinframework-core/src/main/java/org/goblinframework/core/conversion/converter/CalendarToDateConverter.java
@@ -0,0 +1,18 @@
+package org.goblinframework.core.conversion.converter;
+
+import org.goblinframework.api.annotation.Install;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.convert.converter.Converter;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Install
+final public class CalendarToDateConverter implements Converter<Calendar, Date> {
+
+  @NotNull
+  @Override
+  public Date convert(@NotNull Calendar source) {
+    return source.getTime();
+  }
+}
--- goblinframework-dao-mysql/src/test/java/org/goblinframework/dao/mysql/persistence/GoblinStaticPersistenceTest.java
+++ goblinframework-dao-mysql/src/test/java/org/goblinframework/dao/mysql/persistence/GoblinStaticPersistenceTest.java
@@ -1,9 +1,6 @@
 package org.goblinframework.dao.mysql.persistence;
 
-import org.goblinframework.api.annotation.CreateTime;
-import org.goblinframework.api.annotation.Id;
-import org.goblinframework.api.annotation.Revision;
-import org.goblinframework.api.annotation.Table;
+import org.goblinframework.api.annotation.*;
 import org.goblinframework.core.container.SpringManagedBean;
 import org.goblinframework.core.util.RandomUtils;
 import org.goblinframework.dao.mysql.module.test.RebuildMysqlTable;
@@ -17,6 +14,8 @@ import org.springframework.test.context.ContextConfiguration;
 import javax.inject.Inject;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
 
 @RunWith(GoblinTestRunner.class)
 @ContextConfiguration("/UT.xml")
@@ -26,13 +25,23 @@ public class GoblinStaticPersistenceTest extends SpringManagedBean {
   public static class MockData implements Serializable {
     private static final long serialVersionUID = -4568367284300289600L;
 
-    @Id(Id.Generator.AUTO_INC)
-    public Long id;
+    @Id(Id.Generator.AUTO_INC) public Long id;
     public String name;
-    @CreateTime
-    public Instant createTime;
-    @Revision
-    public Integer revision;
+    @CreateTime @Field("CT1") public Instant createTime1;
+    @CreateTime @Field("CT2") public Date createTime2;
+    @CreateTime @Field("UT1") public Calendar updateTime1;
+    @CreateTime @Field("UT2") public Long updateTime2;
+    @CreateTime @Field("UT3") public String updateTime3;
+    @Revision public Integer revision;
+    @Embed public Ext ext;
+  }
+
+  public static class Ext implements Serializable {
+    private static final long serialVersionUID = -4549264762618662638L;
+
+    @Field("F1") public String field1;
+    @Field("F2") public String field2;
+    @Field("F3") public String field3;
   }
 
   @Repository
@@ -47,6 +56,10 @@ public class GoblinStaticPersistenceTest extends SpringManagedBean {
   public void persistence() {
     MockData data = new MockData();
     data.name = RandomUtils.nextObjectId();
+    data.ext = new Ext();
+    data.ext.field1 = "foo";
+    data.ext.field2 = "bar";
+    data.ext.field3 = "gee";
     persistence.__insert(data);
   }
 }
