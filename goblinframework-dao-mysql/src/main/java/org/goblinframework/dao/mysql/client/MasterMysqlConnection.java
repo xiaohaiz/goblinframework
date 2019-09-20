b4816a3dd9bda2a37c841bc35753c9e1be31cdab
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-20 12:51:47 +0800

    org.goblinframework.dao.mysql.client.MasterMysqlConnection

--- goblinframework-dao-mysql/src/main/java/org/goblinframework/dao/mysql/client/MasterMysqlConnection.java
+++ goblinframework-dao-mysql/src/main/java/org/goblinframework/dao/mysql/client/MasterMysqlConnection.java
@@ -1,9 +1,12 @@
 package org.goblinframework.dao.mysql.client;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
@@ -24,23 +27,37 @@ public class MasterMysqlConnection {
     this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
   }
 
+  @NotNull
   public DataSource getDataSource() {
     return dataSource;
   }
 
+  @NotNull
   public PlatformTransactionManager getTransactionManager() {
     return transactionManager;
   }
 
+  @NotNull
   public TransactionTemplate getTransactionTemplate() {
     return transactionTemplate;
   }
 
+  @NotNull
   public JdbcTemplate getJdbcTemplate() {
     return jdbcTemplate;
   }
 
+  @NotNull
   public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
     return namedParameterJdbcTemplate;
+  }
+
+  @Nullable
+  public <E> E executeTransaction(@NotNull TransactionCallback<E> action) {
+    return transactionTemplate.execute(action);
+  }
+
+  public void executeTransactionWithoutResult(@NotNull TransactionCallbackWithoutResult action) {
+    transactionTemplate.execute(action);
   }
 }
