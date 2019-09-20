bb7ce572b4442576bebd22941147b04f343fe8a9
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-20 22:59:15 +0800

    org.goblinframework.core.monitor.StartPoint

---
+++ goblinframework-core/src/main/java/org/goblinframework/core/monitor/StartPoint.java
@@ -0,0 +1,10 @@
+package org.goblinframework.core.monitor;
+
+/**
+ * Monitor flight start point definition.
+ */
+public enum StartPoint {
+
+  GUT,    // GoblinUnitTest
+  PRG     // Programmatic
+}
