d57f13829191c4628a5d52de7a8fdc9dc1ef007f
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-20 16:27:50 +0800

    org.goblinframework.cache.core.support.GoblinCache

---
+++ goblinframework-cache/src/main/java/org/goblinframework/cache/core/support/GoblinCache.java
@@ -0,0 +1,29 @@
+package org.goblinframework.cache.core.support;
+
+import org.goblinframework.cache.core.annotation.CachedExpirationPolicy;
+import org.goblinframework.cache.core.annotation.GoblinCacheExpiration;
+import org.goblinframework.cache.core.cache.CacheSystemLocation;
+import org.goblinframework.cache.core.util.CacheExpirationCalculator;
+
+public class GoblinCache {
+
+  public Class<?> type;
+  public CacheSystemLocation location;
+  public boolean wrapper;
+  public CachedExpirationPolicy expirationPolicy = CachedExpirationPolicy.FIXED;
+  public int expirationValue = GoblinCacheExpiration.DEFAULT_EXPIRATION;
+
+  public org.goblinframework.cache.core.cache.GoblinCache cache;
+
+  public int calculateExpiration() {
+    GoblinCacheExpiration annotation = type.getAnnotation(GoblinCacheExpiration.class);
+    if (annotation != null && annotation.enable()) {
+      return CacheExpirationCalculator.expirationInSeconds(annotation);
+    }
+    return CacheExpirationCalculator.expirationInSeconds(expirationPolicy, expirationValue);
+  }
+
+  public org.goblinframework.cache.core.cache.GoblinCache cache() {
+    return cache;
+  }
+}
