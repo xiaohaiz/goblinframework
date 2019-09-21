02f5e01c3d56b8ea69ee7949d726c5a00bd7431c
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-21 15:50:09 +0800

    org.goblinframework.monitor.module.monitor.FlightPrettyPrinterListener

---
+++ goblinframework-monitor/src/main/java/org/goblinframework/monitor/module/monitor/FlightPrettyPrinterListener.java
@@ -0,0 +1,27 @@
+package org.goblinframework.monitor.module.monitor;
+
+import org.goblinframework.api.annotation.Install;
+import org.goblinframework.api.common.Ordered;
+import org.goblinframework.core.event.GoblinEventContext;
+import org.goblinframework.core.event.GoblinEventListener;
+import org.goblinframework.monitor.flight.FlightEvent;
+import org.jetbrains.annotations.NotNull;
+
+@Install
+final public class FlightPrettyPrinterListener implements GoblinEventListener, Ordered {
+
+  @Override
+  public int getOrder() {
+    return HIGHEST_PRECEDENCE;
+  }
+
+  @Override
+  public boolean accept(@NotNull GoblinEventContext context) {
+    return context.getEvent() instanceof FlightEvent;
+  }
+
+  @Override
+  public void onEvent(@NotNull GoblinEventContext context) {
+    FlightEvent event = (FlightEvent) context.getEvent();
+  }
+}
