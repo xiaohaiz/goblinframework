9f29f4f3917c1e08135f0a649bf58bb1586013f1
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-21 16:25:55 +0800

    org.goblinframework.core.monitor.InstructionEvent

---
+++ goblinframework-core/src/main/java/org/goblinframework/core/monitor/InstructionEvent.java
@@ -0,0 +1,20 @@
+package org.goblinframework.core.monitor;
+
+import org.goblinframework.core.event.GoblinEvent;
+import org.goblinframework.core.event.GoblinEventChannel;
+import org.jetbrains.annotations.NotNull;
+
+@GoblinEventChannel("/goblin/monitor")
+public class InstructionEvent extends GoblinEvent {
+
+  private final Instruction instruction;
+
+  public InstructionEvent(@NotNull Instruction instruction) {
+    this.instruction = instruction;
+  }
+
+  @NotNull
+  public Instruction getInstruction() {
+    return instruction;
+  }
+}
