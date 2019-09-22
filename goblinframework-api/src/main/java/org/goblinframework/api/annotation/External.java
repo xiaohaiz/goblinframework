b7d371c5f306cb9ca2a72f428399d93cd8120771
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-22 22:22:42 +0800

    org.goblinframework.api.service.IServiceInstaller

---
+++ goblinframework-api/src/main/java/org/goblinframework/api/annotation/External.java
@@ -0,0 +1,9 @@
+package org.goblinframework.api.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface External {
+}
---
+++ goblinframework-api/src/main/java/org/goblinframework/api/service/IServiceInstaller.java
@@ -0,0 +1,18 @@
+package org.goblinframework.api.service;
+
+import org.goblinframework.api.annotation.Internal;
+import org.goblinframework.api.annotation.SPI;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+@SPI
+@Internal
+public interface IServiceInstaller {
+
+  <E> List<E> asList(@NotNull Class<E> serviceType);
+
+  <E> E firstOrNull(@NotNull Class<E> serviceType);
+
+
+}
