555d231ea6a21f5fdf76bd6dea7adc4019e3e900
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-22 22:54:07 +0800

    org.goblinframework.core.util.ServiceInstaller

--- goblinframework-core/src/main/java/org/goblinframework/core/util/ServiceInstaller.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/ServiceInstaller.java
@@ -1,23 +1,78 @@
 package org.goblinframework.core.util;
 
 import org.goblinframework.api.annotation.Install;
+import org.goblinframework.api.annotation.Singleton;
+import org.goblinframework.api.annotation.ThreadSafe;
 import org.goblinframework.api.common.Ordered;
+import org.goblinframework.api.service.GoblinServiceException;
 import org.goblinframework.api.service.IServiceInstaller;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-abstract public class ServiceInstaller {
+@Singleton
+@ThreadSafe
+final public class ServiceInstaller implements IServiceInstaller {
 
   @NotNull
   public static <E> List<E> installedList(@NotNull Class<E> serviceType) {
-    return ServiceLoaderImpl.INSTANCE.asList(serviceType);
+    return INSTANCE.asList(serviceType);
   }
 
   @Nullable
   public static <E> E installedFirst(@NotNull Class<E> serviceType) {
-    return ServiceLoaderImpl.INSTANCE.firstOrNull(serviceType);
+    return INSTANCE.firstOrNull(serviceType);
+  }
+
+  private static final ServiceInstaller INSTANCE = new ServiceInstaller();
+
+  private ServiceInstaller() {
+  }
+
+  private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+  private final Map<Class<?>, Object> buffer = new IdentityHashMap<>(64);
+
+  @NotNull
+  @Override
+  @SuppressWarnings("unchecked")
+  public <E> List<E> asList(@NotNull Class<E> serviceType) {
+    if (!serviceType.isInterface()) {
+      throw new GoblinServiceException("Service type must be interface");
+    }
+    lock.readLock().lock();
+    try {
+      Object cached = buffer.get(serviceType);
+      if (cached != null) {
+        return (List<E>) cached;
+      }
+    } finally {
+      lock.readLock().unlock();
+    }
+
+    lock.writeLock().lock();
+    try {
+      Object cached = buffer.get(serviceType);
+      if (cached != null) {
+        return (List<E>) cached;
+      }
+      List<E> installed = new LinkedList<>();
+      ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
+      ServiceLoader.load(serviceType, classLoader).forEach(installed::add);
+      installed.sort(Comparator.comparingInt(ObjectUtils::calculateOrder));
+      installed = Collections.unmodifiableList(installed);
+      buffer.put(serviceType, installed);
+      return installed;
+    } finally {
+      lock.writeLock().unlock();
+    }
+  }
+
+  @Nullable
+  @Override
+  public <E> E firstOrNull(@NotNull Class<E> serviceType) {
+    return asList(serviceType).stream().findFirst().orElse(null);
   }
 
   @Install
@@ -31,13 +86,13 @@ abstract public class ServiceInstaller {
     @NotNull
     @Override
     public <E> List<E> asList(@NotNull Class<E> serviceType) {
-      return ServiceLoaderImpl.INSTANCE.asList(serviceType);
+      return INSTANCE.asList(serviceType);
     }
 
     @Nullable
     @Override
     public <E> E firstOrNull(@NotNull Class<E> serviceType) {
-      return ServiceLoaderImpl.INSTANCE.firstOrNull(serviceType);
+      return INSTANCE.firstOrNull(serviceType);
     }
   }
 }
