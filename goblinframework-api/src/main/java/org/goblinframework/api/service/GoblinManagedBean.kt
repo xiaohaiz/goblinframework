7da3197b54bb7aaace1404af36990bcb75f1ad7d
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 07:19:29 +0800

    org.goblinframework.api.service.GoblinManagedBean

---
+++ goblinframework-api/src/main/java/org/goblinframework/api/service/GoblinManagedBean.kt
@@ -0,0 +1,8 @@
+package org.goblinframework.api.service
+
+@MustBeDocumented
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class GoblinManagedBean(val type: String = "",
+                                   val name: String = "",
+                                   val register: Boolean = true)
