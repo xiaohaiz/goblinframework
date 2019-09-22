c2fd44374b2378b831d72f42b8bb92c5ca94cc82
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-22 22:48:32 +0800

    org.goblinframework.core.util.ServiceLoaderImpl

--- goblinframework-core/src/main/java/org/goblinframework/core/util/ServiceInstaller.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/ServiceInstaller.java
@@ -1,16 +1,12 @@
 package org.goblinframework.core.util;
 
 import org.goblinframework.api.annotation.Install;
-import org.goblinframework.api.annotation.Singleton;
-import org.goblinframework.api.annotation.ThreadSafe;
 import org.goblinframework.api.common.Ordered;
-import org.goblinframework.api.service.GoblinServiceException;
 import org.goblinframework.api.service.IServiceInstaller;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.*;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.List;
 
 abstract public class ServiceInstaller {
 
@@ -22,57 +18,6 @@ abstract public class ServiceInstaller {
   @Nullable
   public static <E> E installedFirst(@NotNull Class<E> serviceType) {
     return ServiceLoaderImpl.INSTANCE.firstOrNull(serviceType);
-  }
-
-  @Singleton
-  @ThreadSafe
-  private static class ServiceLoaderImpl implements IServiceInstaller {
-
-    private static final ServiceLoaderImpl INSTANCE = new ServiceLoaderImpl();
-
-    private static final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-    private static final Map<Class<?>, Object> buffer = new IdentityHashMap<>(64);
-
-    @NotNull
-    @Override
-    @SuppressWarnings("unchecked")
-    public <E> List<E> asList(@NotNull Class<E> serviceType) {
-      if (!serviceType.isInterface()) {
-        throw new GoblinServiceException("Service type must be interface");
-      }
-      lock.readLock().lock();
-      try {
-        Object cached = buffer.get(serviceType);
-        if (cached != null) {
-          return (List<E>) cached;
-        }
-      } finally {
-        lock.readLock().unlock();
-      }
-
-      lock.writeLock().lock();
-      try {
-        Object cached = buffer.get(serviceType);
-        if (cached != null) {
-          return (List<E>) cached;
-        }
-        List<E> installed = new LinkedList<>();
-        ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
-        ServiceLoader.load(serviceType, classLoader).forEach(installed::add);
-        installed.sort(Comparator.comparingInt(ObjectUtils::calculateOrder));
-        installed = Collections.unmodifiableList(installed);
-        buffer.put(serviceType, installed);
-        return installed;
-      } finally {
-        lock.writeLock().unlock();
-      }
-    }
-
-    @Nullable
-    @Override
-    public <E> E firstOrNull(@NotNull Class<E> serviceType) {
-      return asList(serviceType).stream().findFirst().orElse(null);
-    }
   }
 
   @Install
