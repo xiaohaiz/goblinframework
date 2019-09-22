9689e74f1e8f27e3477d1bcd37ed76f2eb08f8d4
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-22 23:18:57 +0800

    org.goblinframework.api.system.IGoblinSystem

--- goblinframework-api/src/main/java/org/goblinframework/api/system/IGoblinSystem.java
+++ goblinframework-api/src/main/java/org/goblinframework/api/system/IGoblinSystem.java
@@ -2,6 +2,8 @@ package org.goblinframework.api.system;
 
 import org.goblinframework.api.annotation.Internal;
 import org.goblinframework.api.annotation.SPI;
+import org.goblinframework.api.service.IServiceInstaller;
+import org.jetbrains.annotations.NotNull;
 
 @SPI
 @Internal
@@ -11,4 +13,12 @@ public interface IGoblinSystem {
 
   void uninstall();
 
+  @NotNull
+  static IGoblinSystem instance() {
+    IGoblinSystem system = IServiceInstaller.instance().firstOrNull(IGoblinSystem.class);
+    if (system == null) {
+      throw new GoblinSystemException("No IGoblinSystem installed");
+    }
+    return system;
+  }
 }
