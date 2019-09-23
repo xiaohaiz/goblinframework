acb6c6372648a5497376db85248fe36fb8529892
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 12:49:23 +0800

    org.goblinframework.api.system.GoblinModule org.goblinframework.api.system.GoblinSubModule

---
+++ goblinframework-api/src/main/java/org/goblinframework/api/system/GoblinModule.java
@@ -0,0 +1,18 @@
+package org.goblinframework.api.system;
+
+public enum GoblinModule {
+  CORE,
+  MONITOR,
+  HTTP,
+  WEBMVC,
+  EMBEDDED,
+  CACHE,
+  DAO,
+  TRANSPORT,
+  REGISTRY,
+  REMOTE,
+  SCHEDULE,
+  MANAGEMENT,
+  BOOTSTRAP,
+  SYSTEM
+}
---
+++ goblinframework-api/src/main/java/org/goblinframework/api/system/GoblinSubModule.java
@@ -0,0 +1,31 @@
+package org.goblinframework.api.system;
+
+import org.jetbrains.annotations.NotNull;
+
+public enum GoblinSubModule {
+
+  CACHE_REDIS(GoblinModule.CACHE);
+
+  private final GoblinModule parent;
+
+  GoblinSubModule(@NotNull GoblinModule parent) {
+    this.parent = parent;
+  }
+
+  @NotNull
+  public GoblinModule parent() {
+    return parent;
+  }
+
+  @NotNull
+  public String fullName() {
+    int idx = name().indexOf("_");
+    String name;
+    if (idx == -1) {
+      name = name();
+    } else {
+      name = name().substring(idx + 1);
+    }
+    return parent.name() + ":" + name;
+  }
+}
