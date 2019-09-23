934d5362721e9a308355a381cb2cfffcbe6f86d1
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 15:30:44 +0800

    org.goblinframework.cache.core.cache.ManagedCacheBuilder

---
+++ goblinframework-cache/src/main/java/org/goblinframework/cache/core/cache/ManagedCacheBuilder.kt
@@ -0,0 +1,47 @@
+package org.goblinframework.cache.core.cache
+
+import org.apache.commons.lang3.mutable.MutableObject
+import org.goblinframework.api.annotation.ThreadSafe
+import org.goblinframework.api.cache.Cache
+import org.goblinframework.api.cache.CacheBuilder
+import org.goblinframework.api.service.GoblinManagedBean
+import org.goblinframework.api.service.GoblinManagedObject
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
+
+@ThreadSafe
+@GoblinManagedBean(type = "cache")
+class ManagedCacheBuilder
+internal constructor(private val delegator: CacheBuilder)
+  : GoblinManagedObject(), CacheBuilderMXBean, CacheBuilder {
+
+  private val lock = ReentrantLock()
+  private val buffer = ConcurrentHashMap<String, MutableObject<ManagedCache>>()
+
+  override fun cache(name: String): Cache? {
+    val id = delegator.decorateCacheName(name)
+    var ref = buffer[id]
+    ref?.run { return value }
+    lock.withLock {
+      ref = buffer[id]
+      ref?.run { return value }
+      val cache = delegator.cache(id)
+      cache?.run {
+        val ret = ManagedCache(this)
+        buffer[id] = MutableObject(ret)
+        return ret
+      } ?: kotlin.run {
+        buffer[id] = MutableObject<ManagedCache>(null)
+        return null
+      }
+    }
+  }
+
+  override fun disposeBean() {
+    lock.withLock {
+      buffer.values.mapNotNull { it.value }.forEach { it.dispose() }
+      buffer.clear()
+    }
+  }
+}
