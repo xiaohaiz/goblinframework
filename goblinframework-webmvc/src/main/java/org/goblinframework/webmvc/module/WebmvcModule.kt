f9508cc5947a10219ae86f7b7d416727b246e9d8
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 13:58:09 +0800

    org.goblinframework.webmvc.module.WebmvcModule

--- goblinframework-webmvc/src/main/java/org/goblinframework/webmvc/module/WebmvcModule.kt
+++ goblinframework-webmvc/src/main/java/org/goblinframework/webmvc/module/WebmvcModule.kt
@@ -1,26 +1,29 @@
 package org.goblinframework.webmvc.module
 
-import org.goblinframework.core.bootstrap.GoblinModule
-import org.goblinframework.core.bootstrap.GoblinModuleFinalizeContext
-import org.goblinframework.core.bootstrap.GoblinModuleInstallContext
+import org.goblinframework.api.annotation.Install
+import org.goblinframework.api.system.GoblinModule
+import org.goblinframework.api.system.IModule
+import org.goblinframework.api.system.ModuleFinalizeContext
+import org.goblinframework.api.system.ModuleInstallContext
 import org.goblinframework.webmvc.handler.RequestHandlerManagerBuilder
 import org.goblinframework.webmvc.module.management.WebmvcManagementController
 
-class WebmvcModule : GoblinModule {
+@Install
+class WebmvcModule : IModule {
 
-  override fun name(): String {
-    return "WEBMVC"
+  override fun id(): GoblinModule {
+    return GoblinModule.WEBMVC
   }
 
   override fun managementEntrance(): String? {
     return "/webmvc/index.do"
   }
 
-  override fun install(ctx: GoblinModuleInstallContext) {
+  override fun install(ctx: ModuleInstallContext) {
     ctx.registerManagementController(WebmvcManagementController.INSTANCE)
   }
 
-  override fun finalize(ctx: GoblinModuleFinalizeContext) {
+  override fun finalize(ctx: ModuleFinalizeContext) {
     RequestHandlerManagerBuilder.INSTANCE.dispose()
   }
 }
