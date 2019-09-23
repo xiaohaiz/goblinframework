91a39c687066d107d13d3cb793d65b7847d12816
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-24 00:05:58 +0800

    org.goblinframework.api.registry.RegistryLocation

--- goblinframework-api/src/main/java/org/goblinframework/api/registry/Registry.java
+++ goblinframework-api/src/main/java/org/goblinframework/api/registry/Registry.java
@@ -1,4 +1,19 @@
 package org.goblinframework.api.registry;
 
+import org.jetbrains.annotations.NotNull;
+
 public interface Registry {
+
+  @NotNull
+  RegistryLocation location();
+
+  @NotNull
+  default RegistrySystem system() {
+    return location().system;
+  }
+
+  @NotNull
+  default String name() {
+    return location().name;
+  }
 }
