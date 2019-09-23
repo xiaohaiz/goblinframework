7fa9ebd217210261258970122f5ab786bcdbb5bf
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 22:03:47 +0800

    org.goblinframework.registry.zookeeper.module.config.ZookeeperConfigManagerTest

--- goblinframework-registry-zookeeper/src/main/java/org/goblinframework/registry/zookeeper/module/RegistryZookeeperModule.kt
+++ goblinframework-registry-zookeeper/src/main/java/org/goblinframework/registry/zookeeper/module/RegistryZookeeperModule.kt
@@ -1,7 +1,10 @@
 package org.goblinframework.registry.zookeeper.module
 
 import org.goblinframework.api.common.Install
-import org.goblinframework.api.system.*
+import org.goblinframework.api.system.GoblinSubModule
+import org.goblinframework.api.system.ISubModule
+import org.goblinframework.api.system.ModuleFinalizeContext
+import org.goblinframework.api.system.ModuleInstallContext
 import org.goblinframework.registry.zookeeper.client.ZkTranscoderManager
 import org.goblinframework.registry.zookeeper.module.config.ZookeeperConfigManager
 
@@ -14,10 +17,6 @@ class RegistryZookeeperModule : ISubModule {
 
   override fun install(ctx: ModuleInstallContext) {
     ctx.registerConfigParser(ZookeeperConfigManager.INSTANCE.configParser)
-  }
-
-  override fun initialize(ctx: ModuleInitializeContext) {
-    ZookeeperConfigManager.INSTANCE.initialize()
   }
 
   override fun finalize(ctx: ModuleFinalizeContext) {
