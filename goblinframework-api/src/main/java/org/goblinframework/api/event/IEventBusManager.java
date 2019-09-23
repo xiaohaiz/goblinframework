e506b6af33d859592b66c34c3f64e944e2aa80bb
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 21:18:42 +0800

    org.goblinframework.core.event.EventBusManager

--- goblinframework-api/src/main/java/org/goblinframework/api/event/IEventBusManager.java
+++ goblinframework-api/src/main/java/org/goblinframework/api/event/IEventBusManager.java
@@ -6,6 +6,23 @@ import org.jetbrains.annotations.NotNull;
 @Internal
 public interface IEventBusManager {
 
+  void subscribe(@NotNull GoblinEventListener listener);
+
+  void subscribe(@NotNull String channel, @NotNull GoblinEventListener listener);
+
+  void unsubscribe(@NotNull GoblinEventListener listener);
+
+  void unsubscribe(@NotNull String channel, @NotNull GoblinEventListener listener);
+
+  @NotNull
+  GoblinEventFuture publish(@NotNull GoblinEvent event);
+
+  @NotNull
+  GoblinEventFuture publish(@NotNull String channel, @NotNull GoblinEvent event);
+
+  @NotNull
+  <E> GoblinCallbackFuture<E> execute(@NotNull GoblinCallback<E> callback);
+
   @NotNull
   static IEventBusManager instance() {
     IEventBusManager manager = EventBusManagerInstaller.INSTALLED;
--- goblinframework-core/src/main/java/org/goblinframework/core/event/EventBusManager.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/event/EventBusManager.kt
@@ -2,12 +2,56 @@ package org.goblinframework.core.event
 
 import org.goblinframework.api.annotation.Install
 import org.goblinframework.api.common.Ordered
-import org.goblinframework.api.event.IEventBusManager
+import org.goblinframework.api.event.*
+import org.goblinframework.core.event.boss.EventBusBoss
+import org.goblinframework.core.event.dsl.GoblinCallbackEvent
 
 @Install
 class EventBusManager : IEventBusManager, Ordered {
 
   override fun getOrder(): Int {
     return Ordered.HIGHEST_PRECEDENCE
+  }
+
+  override fun subscribe(listener: GoblinEventListener) {
+    EventBusBoss.INSTANCE.subscribe(listener)
+  }
+
+  override fun subscribe(channel: String, listener: GoblinEventListener) {
+    EventBusBoss.INSTANCE.subscribe(channel, listener)
+  }
+
+  override fun unsubscribe(listener: GoblinEventListener) {
+    EventBusBoss.INSTANCE.unsubscribe(listener)
+  }
+
+  override fun unsubscribe(channel: String, listener: GoblinEventListener) {
+    EventBusBoss.INSTANCE.unsubscribe(channel, listener)
+  }
+
+  override fun publish(event: GoblinEvent): GoblinEventFuture {
+    return EventBusBoss.INSTANCE.publish(event)
+  }
+
+  override fun publish(channel: String, event: GoblinEvent): GoblinEventFuture {
+    return EventBusBoss.INSTANCE.publish(channel, event)
+  }
+
+  override fun <E : Any?> execute(callback: GoblinCallback<E>): GoblinCallbackFuture<E> {
+    val future = GoblinCallbackFuture<E>()
+    val event = GoblinCallbackEvent(callback)
+    publish(event).addListener {
+      val context: GoblinEventContext
+      try {
+        context = it.uninterruptibly
+      } catch (ex: Throwable) {
+        future.complete(null, ex)
+        return@addListener
+      }
+      @Suppress("UNCHECKED_CAST")
+      val result = context.getExtension("GoblinCallback.Result") as E?
+      future.complete(result)
+    }
+    return future
   }
 }
