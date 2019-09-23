834d5d68abf08e8d71ff9d7c907083a25caedd65
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 13:01:34 +0800

    Module related refactorings.

--- goblinframework-core/src/main/java/org/goblinframework/core/system/GoblinSystemImpl.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/system/GoblinSystemImpl.kt
@@ -2,11 +2,13 @@ package org.goblinframework.core.system
 
 import org.goblinframework.api.service.GoblinManagedBean
 import org.goblinframework.api.service.GoblinManagedObject
-import org.goblinframework.core.bootstrap.GoblinModuleManager
+import org.goblinframework.api.system.GoblinModule
 import org.goblinframework.core.config.ConfigLoader
 import org.goblinframework.core.container.SpringContainerManager
+import org.goblinframework.core.event.EventBus
 import org.goblinframework.core.event.boss.EventBusBoss
 import org.goblinframework.core.util.ClassUtils
+import java.util.concurrent.TimeUnit
 
 @GoblinManagedBean(type = "core", name = "GoblinSystem")
 class GoblinSystemImpl internal constructor() : GoblinManagedObject(), GoblinSystemMXBean {
@@ -19,12 +21,50 @@ class GoblinSystemImpl internal constructor() : GoblinManagedObject(), GoblinSys
     ConfigLoader.INSTANCE.initialize()
     EventBusBoss.INSTANCE.initialize()
     ConfigLoader.INSTANCE.start()
-    GoblinModuleManager.INSTANCE.executeInstall().executeInitialize()
+
+    EventBus.subscribe(SubModuleEventListener.INSTANCE)
+
+    for (id in GoblinModule.values()) {
+      val module = ModuleLoader.module(id) ?: continue
+      logger.info("Install {${module.id()}}")
+      module.install(ModuleInstallContextImpl.INSTANCE)
+    }
+    for (module in ExtModuleLoader.asList()) {
+      logger.info("Install {${module.id()}}")
+      module.install(ModuleInstallContextImpl.INSTANCE)
+    }
+
+    for (id in GoblinModule.values()) {
+      val module = ModuleLoader.module(id) ?: continue
+      logger.info("Initialize {${module.id()}}")
+      module.initialize(ModuleInitializeContextImpl.INSTANCE)
+    }
+    for (module in ExtModuleLoader.asList()) {
+      logger.info("Initialize {${module.id()}}")
+      module.initialize(ModuleInitializeContextImpl.INSTANCE)
+    }
   }
 
   override fun disposeBean() {
     SpringContainerManager.INSTANCE.dispose()
-    GoblinModuleManager.INSTANCE.executeFinalize()
+
+    val future = EventBus.execute {
+      for (id in GoblinModule.values().reversed()) {
+        val module = ModuleLoader.module(id) ?: continue
+        logger.info("Finalize {${module.id()}}")
+        module.finalize(ModuleFinalizeContextImpl.INSTANCE)
+      }
+      for (module in ExtModuleLoader.asList().reversed()) {
+        logger.info("Finalize {${module.id()}}")
+        module.finalize(ModuleFinalizeContextImpl.INSTANCE)
+      }
+    }
+    try {
+      future.awaitUninterruptibly(1, TimeUnit.MINUTES)
+    } catch (ignore: Throwable) {
+    }
+    EventBus.unsubscribe(SubModuleEventListener.INSTANCE)
+
     ConfigLoader.INSTANCE.stop()
     EventBusBoss.INSTANCE.dispose()
     ConfigLoader.INSTANCE.dispose()
