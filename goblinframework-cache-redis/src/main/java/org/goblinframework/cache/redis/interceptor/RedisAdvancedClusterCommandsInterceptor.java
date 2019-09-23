44e5152100eab56ee515f7a837ec2cf6b14f2f52
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 18:00:20 +0800

    Interceptors.

---
+++ goblinframework-cache-redis/src/main/java/org/goblinframework/cache/redis/interceptor/RedisAdvancedClusterCommandsInterceptor.java
@@ -0,0 +1,35 @@
+package org.goblinframework.cache.redis.interceptor;
+
+import io.lettuce.core.cluster.api.sync.RedisAdvancedClusterCommands;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.goblinframework.api.monitor.Instruction;
+import org.goblinframework.cache.redis.module.monitor.RDS;
+import org.goblinframework.core.reflection.ReflectionUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Method;
+
+final public class RedisAdvancedClusterCommandsInterceptor implements MethodInterceptor {
+
+  private final RedisAdvancedClusterCommands target;
+
+  public RedisAdvancedClusterCommandsInterceptor(@NotNull RedisAdvancedClusterCommands target) {
+    this.target = target;
+  }
+
+  @Override
+  public Object invoke(MethodInvocation invocation) throws Throwable {
+    Method method = invocation.getMethod();
+    if (ReflectionUtils.isToStringMethod(method)) {
+      return target.toString();
+    }
+    try (RDS rds = new RDS(Instruction.Mode.SYN)) {
+      rds.operation = method.getName();
+
+      Object[] arguments = invocation.getArguments();
+      RedisCommandsMethodPool.extractKeysIfNecessary(rds, method, arguments);
+      return ReflectionUtils.invoke(target, method, arguments);
+    }
+  }
+}
