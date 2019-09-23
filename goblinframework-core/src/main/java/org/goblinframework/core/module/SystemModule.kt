b192da650c29cbeae9d49d40ac9fedd6294dd533
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 13:50:40 +0800

    org.goblinframework.core.module.SystemModule

--- goblinframework-core/src/main/java/org/goblinframework/core/module/SystemModule.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/module/SystemModule.kt
@@ -1,22 +1,25 @@
 package org.goblinframework.core.module
 
+import org.goblinframework.api.annotation.Install
 import org.goblinframework.api.service.ServiceInstaller
-import org.goblinframework.core.bootstrap.GoblinModule
-import org.goblinframework.core.bootstrap.GoblinModuleFinalizeContext
-import org.goblinframework.core.bootstrap.GoblinModuleInitializeContext
+import org.goblinframework.api.system.GoblinModule
+import org.goblinframework.api.system.IModule
+import org.goblinframework.api.system.ModuleFinalizeContext
+import org.goblinframework.api.system.ModuleInitializeContext
 import org.goblinframework.core.module.spi.ManagementServerLifecycle
 
-class SystemModule : GoblinModule {
+@Install
+class SystemModule : IModule {
 
-  override fun name(): String {
-    return "SYSTEM"
+  override fun id(): GoblinModule {
+    return GoblinModule.SYSTEM
   }
 
-  override fun initialize(ctx: GoblinModuleInitializeContext) {
+  override fun initialize(ctx: ModuleInitializeContext) {
     ServiceInstaller.firstOrNull(ManagementServerLifecycle::class.java)?.run { start() }
   }
 
-  override fun finalize(ctx: GoblinModuleFinalizeContext) {
+  override fun finalize(ctx: ModuleFinalizeContext) {
     ServiceInstaller.firstOrNull(ManagementServerLifecycle::class.java)?.run { stop() }
   }
 }
