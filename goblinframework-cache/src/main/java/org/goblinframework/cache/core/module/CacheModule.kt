bba62bdf4fe6d77c2c6fc267e97d499a8fc203c7
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-23 14:27:38 +0800

    org.goblinframework.cache.core.module.CacheModule

--- goblinframework-cache/src/main/java/org/goblinframework/cache/core/module/CacheModule.kt
+++ goblinframework-cache/src/main/java/org/goblinframework/cache/core/module/CacheModule.kt
@@ -1,48 +1,40 @@
 package org.goblinframework.cache.core.module
 
 import org.goblinframework.api.annotation.Install
+import org.goblinframework.api.system.*
 import org.goblinframework.cache.core.cache.GoblinCacheBuilderManager
-import org.goblinframework.cache.core.module.monitor.instruction.VMC
-import org.goblinframework.cache.core.module.monitor.instruction.VMCTranslator
 import org.goblinframework.cache.core.module.test.FlushInJvmCacheBeforeTestMethod
-import org.goblinframework.cache.core.provider.InJvmCacheBuilder
-import org.goblinframework.cache.core.provider.NoOpCacheBuilder
-import org.goblinframework.core.bootstrap.GoblinModule
-import org.goblinframework.core.bootstrap.GoblinModuleFinalizeContext
-import org.goblinframework.core.bootstrap.GoblinModuleInitializeContext
-import org.goblinframework.core.bootstrap.GoblinModuleInstallContext
-import org.goblinframework.core.cache.GoblinCacheSystem
 
 @Install
-class CacheModule : GoblinModule {
+class CacheModule : IModule {
 
-  override fun name(): String {
-    return "CACHE"
+  override fun id(): GoblinModule {
+    return GoblinModule.CACHE
   }
 
-  override fun install(ctx: GoblinModuleInstallContext) {
-    ctx.registerGoblinCacheBuilder(GoblinCacheSystem.JVM, InJvmCacheBuilder.INSTANCE)
-    ctx.registerGoblinCacheBuilder(GoblinCacheSystem.NOP, NoOpCacheBuilder.INSTANCE)
-    ctx.registerInstructionTranslator(VMC::class.java, VMCTranslator.INSTANCE)
+  override fun install(ctx: ModuleInstallContext) {
     ctx.registerTestExecutionListener(FlushInJvmCacheBeforeTestMethod.INSTANCE)
-    ctx.createChildModuleManager()
-        .module("CACHE:COUCHBASE")
-        .module("CACHE:REDIS")
+    //ctx.registerGoblinCacheBuilder(GoblinCacheSystem.JVM, InJvmCacheBuilder.INSTANCE)
+    //ctx.registerGoblinCacheBuilder(GoblinCacheSystem.NOP, NoOpCacheBuilder.INSTANCE)
+    //ctx.registerInstructionTranslator(VMC::class.java, VMCTranslator.INSTANCE)
+    ctx.createSubModules()
+        .module(GoblinSubModule.CACHE_COUCHBASE)
+        .module(GoblinSubModule.CACHE_REDIS)
         .install(ctx)
   }
 
-  override fun initialize(ctx: GoblinModuleInitializeContext) {
-    ctx.createChildModuleManager()
-        .module("CACHE:COUCHBASE")
-        .module("CACHE:REDIS")
+  override fun initialize(ctx: ModuleInitializeContext) {
+    ctx.createSubModules()
+        .module(GoblinSubModule.CACHE_COUCHBASE)
+        .module(GoblinSubModule.CACHE_REDIS)
         .initialize(ctx)
   }
 
-  override fun finalize(ctx: GoblinModuleFinalizeContext) {
+  override fun finalize(ctx: ModuleFinalizeContext) {
     GoblinCacheBuilderManager.INSTANCE.dispose()
-    ctx.createChildModuleManager()
-        .module("CACHE:COUCHBASE")
-        .module("CACHE:REDIS")
+    ctx.createSubModules()
+        .module(GoblinSubModule.CACHE_COUCHBASE)
+        .module(GoblinSubModule.CACHE_REDIS)
         .finalize(ctx)
   }
 }
