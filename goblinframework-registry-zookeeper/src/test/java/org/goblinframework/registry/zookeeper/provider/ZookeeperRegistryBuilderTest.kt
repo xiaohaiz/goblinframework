0baf306491bac2d4e6e52dab3b94c2de2bd30379
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-24 12:13:03 +0800

    org.goblinframework.registry.zookeeper.provider.ZookeeperRegistryBuilderTest

---
+++ goblinframework-registry-zookeeper/src/test/java/org/goblinframework/registry/zookeeper/provider/ZookeeperRegistryBuilderTest.kt
@@ -0,0 +1,22 @@
+package org.goblinframework.registry.zookeeper.provider
+
+import org.goblinframework.api.registry.RegistrySystem
+import org.goblinframework.registry.core.manager.RegistryBuilderManager
+import org.goblinframework.test.runner.GoblinTestRunner
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.ContextConfiguration
+
+@RunWith(GoblinTestRunner::class)
+@ContextConfiguration("/UT.xml")
+class ZookeeperRegistryBuilderTest {
+
+  @Test
+  fun getRegistry() {
+    val builder = RegistryBuilderManager.INSTANCE.getRegistryBuilder(RegistrySystem.ZKP)
+    assertNotNull(builder)
+    val registry = builder?.getRegistry("_ut")
+    assertNotNull(registry)
+  }
+}
