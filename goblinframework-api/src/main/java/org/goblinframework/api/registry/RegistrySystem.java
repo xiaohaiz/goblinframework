371c0db7022ccabab4c9d65966462ea954478647
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-24 12:16:23 +0800

    org.goblinframework.api.registry.RegistrySystem

--- goblinframework-api/src/main/java/org/goblinframework/api/registry/RegistrySystem.java
+++ goblinframework-api/src/main/java/org/goblinframework/api/registry/RegistrySystem.java
@@ -1,7 +1,22 @@
 package org.goblinframework.api.registry;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public enum RegistrySystem {
 
-  ZKP
+  ZKP;
+
+  @Nullable
+  public RegistryBuilder getRegistryBuilder() {
+    IRegistryBuilderManager builderManager = IRegistryBuilderManager.instance();
+    return builderManager == null ? null : builderManager.getRegistryBuilder(this);
+  }
+
+  @Nullable
+  public Registry getRegistry(@NotNull String name) {
+    RegistryBuilder builder = getRegistryBuilder();
+    return builder == null ? null : builder.getRegistry(name);
+  }
 
 }
