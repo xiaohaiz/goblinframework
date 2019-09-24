51e23e335a2e6f7e84505dcc20d702141b903e12
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-24 17:36:33 +0800

    org.goblinframework.schedule.cron.CronTaskManagerTest

---
+++ goblinframework-api/src/main/java/org/goblinframework/api/schedule/CronConstants.java
@@ -0,0 +1,8 @@
+package org.goblinframework.api.schedule;
+
+final public class CronConstants {
+
+  public static final String SECOND_TIMER = "*/1 * * * * ?";
+  public static final String MINUTE_TIMER = "0 0/1 * * * ?";
+
+}
---
+++ goblinframework-schedule/src/test/java/org/goblinframework/schedule/cron/CronTaskManagerTest.kt
@@ -0,0 +1,36 @@
+package org.goblinframework.schedule.cron
+
+import org.goblinframework.api.schedule.CronConstants
+import org.goblinframework.api.schedule.CronTask
+import org.goblinframework.core.util.RandomUtils
+import org.goblinframework.test.runner.GoblinTestRunner
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.ContextConfiguration
+import java.util.concurrent.CountDownLatch
+
+@RunWith(GoblinTestRunner::class)
+@ContextConfiguration("/UT.xml")
+class CronTaskManagerTest {
+
+  @Test
+  fun cronTask() {
+    val latch = CountDownLatch(1)
+    val task = object : CronTask {
+      override fun name(): String {
+        return RandomUtils.nextObjectId()
+      }
+
+      override fun cronExpression(): String {
+        return CronConstants.SECOND_TIMER
+      }
+
+      override fun execute() {
+        latch.countDown()
+      }
+    }
+    CronTaskManager.INSTANCE.register(task)
+    latch.await()
+    CronTaskManager.INSTANCE.unregister(task.name())
+  }
+}
