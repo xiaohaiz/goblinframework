bd263b388443138265d88b1229955e69d0dc228b
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-24 17:59:53 +0800

    Mark org.goblinframework.core.module.spi.GoblinTimerEventGenerator deprecated.

--- goblinframework-core/src/main/java/org/goblinframework/core/module/spi/GoblinTimerEventGenerator.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/module/spi/GoblinTimerEventGenerator.kt
@@ -3,5 +3,6 @@ package org.goblinframework.core.module.spi
 import org.goblinframework.api.common.Lifecycle
 import org.goblinframework.api.common.Ordered
 
+@Deprecated("TBR")
 interface GoblinTimerEventGenerator : Lifecycle, Ordered {
 }
