14c9b50c96f3e11dff34586dec246fb6a15ed556
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-25 11:46:03 +0800

    org.goblinframework.schedule.cron.CronTaskManager

--- goblinframework-schedule/src/main/java/org/goblinframework/schedule/cron/CronTaskManager.kt
+++ goblinframework-schedule/src/main/java/org/goblinframework/schedule/cron/CronTaskManager.kt
@@ -1,28 +1,28 @@
 package org.goblinframework.schedule.cron
 
 import org.goblinframework.api.common.Install
+import org.goblinframework.api.common.Singleton
+import org.goblinframework.api.common.ThreadSafe
 import org.goblinframework.api.schedule.CronTask
 import org.goblinframework.api.schedule.ICronTaskManager
 import org.goblinframework.api.service.GoblinManagedBean
 import org.goblinframework.api.service.GoblinManagedObject
-import org.quartz.CronExpression
-import org.quartz.CronTrigger
-import org.quartz.JobDetail
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean
-import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean
 import org.springframework.scheduling.quartz.SchedulerFactoryBean
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
+@Singleton
+@ThreadSafe
 @GoblinManagedBean(type = "Schedule")
-class CronTaskManager private constructor()
-  : GoblinManagedObject(), ICronTaskManager, CronTaskManagerMXBean {
+class CronTaskManager private constructor() : GoblinManagedObject(), ICronTaskManager, CronTaskManagerMXBean {
 
   companion object {
     @JvmField val INSTANCE = CronTaskManager()
   }
 
   private val scheduler: SchedulerFactoryBean
-  private val tasks = ConcurrentHashMap<String, Pair<JobDetail, CronTrigger>>()
+  private val lock = ReentrantLock()
+  private val tasks = mutableMapOf<String, ManagedCronTask>()
 
   init {
     scheduler = SchedulerFactoryBean()
@@ -32,43 +32,24 @@ class CronTaskManager private constructor()
     scheduler.start()
   }
 
-  @Synchronized
   override fun register(task: CronTask) {
-    tasks[task.name()]?.run { return }
-
-    val cronExpression = CronExpression(task.cronExpression())
-
-    val jobDetailFactoryBean = MethodInvokingJobDetailFactoryBean()
-    jobDetailFactoryBean.setName("JobDetail-${task.name()}")
-    jobDetailFactoryBean.targetObject = task
-    jobDetailFactoryBean.targetMethod = "execute"
-    jobDetailFactoryBean.setConcurrent(true)
-    jobDetailFactoryBean.afterPropertiesSet()
-    val jobDetail = jobDetailFactoryBean.getObject()!!
-
-    val triggerFactoryBean = CronTriggerFactoryBean()
-    triggerFactoryBean.setName("CronTrigger-${task.name()}")
-    triggerFactoryBean.setJobDetail(jobDetail)
-    triggerFactoryBean.setCronExpression(cronExpression.cronExpression)
-    triggerFactoryBean.afterPropertiesSet()
-    val cronTrigger = triggerFactoryBean.getObject()
-
-    tasks[task.name()] = Pair(jobDetail, cronTrigger)
-
-    scheduler.getObject()?.let {
-      it.addJob(jobDetail, true)
-      it.scheduleJob(cronTrigger)
+    lock.withLock {
+      tasks[task.name()]?.run { return }
+      val mct = ManagedCronTask(scheduler.getObject()!!, task)
+      mct.initialize()
+      tasks[task.name()] = mct
     }
   }
 
   override fun unregister(name: String) {
-    tasks.remove(name)?.let {
-      scheduler.getObject()?.unscheduleJob(it.second.key)
-      scheduler.getObject()?.deleteJob(it.first.key)
-    }
+    lock.withLock { tasks.remove(name) }?.dispose()
   }
 
   override fun disposeBean() {
+    lock.withLock {
+      tasks.values.forEach { it.dispose() }
+      tasks.clear()
+    }
     scheduler.stop()
     scheduler.destroy()
   }
