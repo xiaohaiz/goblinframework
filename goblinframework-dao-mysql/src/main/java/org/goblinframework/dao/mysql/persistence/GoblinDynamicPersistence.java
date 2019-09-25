cf0334c91481ea37eaa76e3adf87eeb0dcf1aadb
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-25 11:47:38 +0800

    org.goblinframework.dao.mysql.persistence.GoblinDynamicPersistence

---
+++ goblinframework-dao-mysql/src/main/java/org/goblinframework/dao/mysql/persistence/GoblinDynamicPersistence.java
@@ -0,0 +1,6 @@
+package org.goblinframework.dao.mysql.persistence;
+
+import org.goblinframework.dao.mysql.support.MysqlPersistenceSupport;
+
+abstract public class GoblinDynamicPersistence<E, ID> extends MysqlPersistenceSupport<E, ID> {
+}
