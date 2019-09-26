b358aad573d90c30aed0d59215b667ac4d2372e8
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-26 13:48:01 +0800

    org.goblinframework.remote.server.service.ExposeServiceScanner

--- goblinframework-remote-server/src/main/java/org/goblinframework/remote/server/service/ExposeServiceScanner.java
+++ goblinframework-remote-server/src/main/java/org/goblinframework/remote/server/service/ExposeServiceScanner.java
@@ -1,21 +1,22 @@
 package org.goblinframework.remote.server.service;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.goblinframework.api.common.GoblinInterruptedException;
 import org.goblinframework.core.container.ContainerManagedBean;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationContext;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 final public class ExposeServiceScanner {
 
   public static void scan(@NotNull ApplicationContext applicationContext) {
-    Semaphore semaphore = new Semaphore(8);
-    ExecutorService executorService = Executors.newCachedThreadPool();
+    List<ImmutablePair<ContainerManagedBean, List<ExposeServiceId>>> candidates = new ArrayList<>();
     String[] beanNames = applicationContext.getBeanNamesForType(Object.class, true, false);
     for (String beanName : beanNames) {
       Class<?> beanType = applicationContext.getType(beanName);
@@ -26,19 +27,21 @@ final public class ExposeServiceScanner {
       if (ids.isEmpty()) {
         continue;
       }
-      semaphore.acquireUninterruptibly();
-      executorService.submit(() -> {
-        try {
-          ContainerManagedBean cmb = new ContainerManagedBean(beanName, applicationContext);
-          RemoteServiceManager.INSTANCE.createManagedService(cmb, ids);
-        } finally {
-          semaphore.release();
-        }
-      });
+      candidates.add(ImmutablePair.of(new ContainerManagedBean(beanName, applicationContext), ids));
     }
-    executorService.shutdown();
+    CountDownLatch latch = new CountDownLatch(candidates.size());
+    ExecutorService executorService = Executors.newFixedThreadPool(8);
+    candidates.forEach(c -> executorService.submit(() -> {
+      try {
+        RemoteServiceManager.INSTANCE.createManagedService(c.left, c.right);
+      } finally {
+        latch.countDown();
+      }
+    }));
     try {
-      executorService.awaitTermination(3, TimeUnit.SECONDS);
+      latch.await();
+      executorService.shutdown();
+      executorService.awaitTermination(1, TimeUnit.SECONDS);
     } catch (InterruptedException ex) {
       throw new GoblinInterruptedException(ex);
     }
