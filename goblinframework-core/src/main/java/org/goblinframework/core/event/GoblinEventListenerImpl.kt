901281fd3e8c4de0e2d3d32287ed4cc5d955d20a
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-26 10:48:39 +0800

    org.goblinframework.core.event.GoblinEventListenerMXBean

--- goblinframework-core/src/main/java/org/goblinframework/core/event/GoblinEventListenerImpl.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/event/GoblinEventListenerImpl.kt
@@ -6,21 +6,65 @@ import org.goblinframework.api.event.GoblinEventListener
 import org.goblinframework.api.service.GoblinManagedBean
 import org.goblinframework.api.service.GoblinManagedObject
 import org.goblinframework.core.util.ObjectUtils
+import org.goblinframework.core.util.StopWatch
+import java.util.concurrent.atomic.LongAdder
 
 @GoblinManagedBean(type = "Core", name = "GoblinEventListener")
 class GoblinEventListenerImpl
 internal constructor(private val delegator: GoblinEventListener)
   : GoblinManagedObject(), GoblinEventListener, Ordered, GoblinEventListenerMXBean {
 
+  private val watch = StopWatch()
+  private val acceptedCount = LongAdder()
+  private val rejectedCount = LongAdder()
+  private val succeedCount = LongAdder()
+  private val failedCount = LongAdder()
+
   override fun getOrder(): Int {
     return ObjectUtils.calculateOrder(delegator)
   }
 
   override fun accept(context: GoblinEventContext): Boolean {
-    return delegator.accept(context)
+    val ret = delegator.accept(context)
+    if (ret) acceptedCount.increment() else rejectedCount.increment()
+    return ret
   }
 
   override fun onEvent(context: GoblinEventContext) {
-    delegator.onEvent(context)
+    try {
+      delegator.onEvent(context)
+      succeedCount.increment()
+    } catch (ex: Exception) {
+      failedCount.increment()
+      throw ex
+    }
+  }
+
+  override fun disposeBean() {
+    watch.stop()
+  }
+
+  override fun getUpTime(): String {
+    return watch.toString()
+  }
+
+  override fun getListener(): String {
+    return delegator.toString()
+  }
+
+  override fun getAcceptedCount(): Long {
+    return acceptedCount.sum()
+  }
+
+  override fun getRejectedCount(): Long {
+    return rejectedCount.sum()
+  }
+
+  override fun getSucceedCount(): Long {
+    return succeedCount.sum()
+  }
+
+  override fun getFailedCount(): Long {
+    return failedCount.sum()
   }
 }
--- goblinframework-core/src/main/java/org/goblinframework/core/event/GoblinEventListenerMXBean.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/event/GoblinEventListenerMXBean.kt
@@ -2,4 +2,17 @@ package org.goblinframework.core.event
 
 import java.lang.management.PlatformManagedObject
 
-interface GoblinEventListenerMXBean : PlatformManagedObject
+interface GoblinEventListenerMXBean : PlatformManagedObject {
+
+  fun getUpTime(): String
+
+  fun getListener(): String
+
+  fun getAcceptedCount(): Long
+
+  fun getRejectedCount(): Long
+
+  fun getSucceedCount(): Long
+
+  fun getFailedCount(): Long
+}
