93b394ed4a09ab5fb2ee99eee6f8e9895cf411f8
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-27 21:57:29 +0800

    org.goblinframework.database.mongo.bson.deserializer.BsonObjectIdDeserializer

---
+++ goblinframework-database-mongo/src/main/java/org/goblinframework/database/mongo/bson/deserializer/BsonObjectIdDeserializer.java
@@ -0,0 +1,27 @@
+package org.goblinframework.database.mongo.bson.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.bson.types.ObjectId;
+
+import java.io.IOException;
+
+public class BsonObjectIdDeserializer extends JsonDeserializer<ObjectId> {
+
+  @SuppressWarnings("deprecation")
+  @Override
+  public ObjectId deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
+    Object object = p.getEmbeddedObject();
+    if (object instanceof de.undercouch.bson4jackson.types.ObjectId) {
+      int t = ((de.undercouch.bson4jackson.types.ObjectId) object).getTime();
+      int m = ((de.undercouch.bson4jackson.types.ObjectId) object).getMachine();
+      int i = ((de.undercouch.bson4jackson.types.ObjectId) object).getInc();
+      return ObjectId.createFromLegacyFormat(t, m, i);
+    }
+    if (object instanceof String && ObjectId.isValid((String) object)) {
+      return new ObjectId((String) object);
+    }
+    return null;
+  }
+}
