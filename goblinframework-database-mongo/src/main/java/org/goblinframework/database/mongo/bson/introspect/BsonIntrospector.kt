b7baf95768065a7f1a616b40db14071ce163b324
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-27 21:59:38 +0800

    org.goblinframework.database.mongo.bson.introspect.BsonIntrospector

--- goblinframework-database-mongo/src/main/java/org/goblinframework/database/mongo/bson/introspect/BsonIntrospector.kt
+++ goblinframework-database-mongo/src/main/java/org/goblinframework/database/mongo/bson/introspect/BsonIntrospector.kt
@@ -1,5 +1,14 @@
 package org.goblinframework.database.mongo.bson.introspect
 
+import com.fasterxml.jackson.databind.introspect.Annotated
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector
+import org.goblinframework.api.core.Ignore
 
-class BsonIntrospector : JacksonAnnotationIntrospector()
+class BsonIntrospector : JacksonAnnotationIntrospector() {
+
+  override fun _isIgnorable(a: Annotated?): Boolean {
+    return _findAnnotation(a, Ignore::class.java)?.run { true }
+        ?: super._isIgnorable(a)
+  }
+
+}
