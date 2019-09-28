7f884b4e4a8f0f2e88b87f830e88a50082ed57d6
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-28 17:10:19 +0800

    org.goblinframework.api.event.EventBus refactorings.

--- goblinframework-core/src/main/java/org/goblinframework/api/event/EventBus.java
+++ goblinframework-core/src/main/java/org/goblinframework/api/event/EventBus.java
@@ -1,45 +1,59 @@
 package org.goblinframework.api.event;
 
+import org.goblinframework.core.event.EventBusBoss;
+import org.goblinframework.core.event.GoblinCallbackEvent;
 import org.jetbrains.annotations.NotNull;
 
 final public class EventBus {
 
   public static void register(@NotNull String channel, int ringBufferSize, int workerHandlers) {
-    IEventBusManager.instance().register(channel, ringBufferSize, workerHandlers);
+    EventBusBoss.INSTANCE.register(channel, ringBufferSize, workerHandlers);
   }
 
   public static void unregister(@NotNull String channel) {
-    IEventBusManager.instance().unregister(channel);
+    EventBusBoss.INSTANCE.unregister(channel);
   }
 
   public static void subscribe(@NotNull GoblinEventListener listener) {
-    IEventBusManager.instance().subscribe(listener);
+    EventBusBoss.INSTANCE.subscribe(listener);
   }
 
   public static void subscribe(@NotNull String channel, @NotNull GoblinEventListener listener) {
-    IEventBusManager.instance().subscribe(channel, listener);
+    EventBusBoss.INSTANCE.subscribe(channel, listener);
   }
 
   public static void unsubscribe(@NotNull GoblinEventListener listener) {
-    IEventBusManager.instance().unsubscribe(listener);
+    EventBusBoss.INSTANCE.unsubscribe(listener);
   }
 
   public static void unsubscribe(@NotNull String channel, @NotNull GoblinEventListener listener) {
-    IEventBusManager.instance().unsubscribe(channel, listener);
+    EventBusBoss.INSTANCE.unsubscribe(channel, listener);
   }
 
   @NotNull
   public static GoblinEventFuture publish(@NotNull GoblinEvent event) {
-    return IEventBusManager.instance().publish(event);
+    return EventBusBoss.INSTANCE.publish(event);
   }
 
   @NotNull
   public static GoblinEventFuture publish(@NotNull String channel, @NotNull GoblinEvent event) {
-    return IEventBusManager.instance().publish(channel, event);
+    return EventBusBoss.INSTANCE.publish(channel, event);
   }
 
   @NotNull
   public static <E> GoblinCallbackFuture<E> execute(@NotNull GoblinCallback<E> callback) {
-    return IEventBusManager.instance().execute(callback);
+    GoblinCallbackFuture<E> future = new GoblinCallbackFuture<>();
+    EventBusBoss.INSTANCE.publish(new GoblinCallbackEvent(callback)).addListener(it -> {
+      GoblinEventContext context;
+      try {
+        context = it.getUninterruptibly();
+      } catch (Throwable ex) {
+        future.complete(null, ex);
+        return;
+      }
+      @SuppressWarnings("unchecked") E result = (E) context.getExtension("GoblinCallback.Result");
+      future.complete(result);
+    });
+    return future;
   }
 }
