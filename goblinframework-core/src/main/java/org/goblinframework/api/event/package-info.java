67202a88f0e430485f66bc28e7eab9907f6b1973
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-28 17:11:05 +0800

    Remove unnecessary classes.

--- goblinframework-core/src/main/java/org/goblinframework/api/event/package-info.java
+++
@@ -1,7 +0,0 @@
-package org.goblinframework.api.event;
-
-import static org.goblinframework.core.service.ServiceInstaller.firstOrNull;
-
-final class EventBusManagerInstaller {
-  static final IEventBusManager INSTALLED = firstOrNull(IEventBusManager.class);
-}
