232e181debf95cb86d2bad413a9695f80526cde9
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-28 16:33:46 +0800

    Remove org.goblinframework.api.core.ApplicationContextProvider

--- goblinframework-core/src/main/java/org/goblinframework/core/container/SpringContainer.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/container/SpringContainer.kt
@@ -1,19 +1,14 @@
 package org.goblinframework.core.container
 
-import org.goblinframework.api.core.ApplicationContextProvider
 import org.goblinframework.core.service.GoblinManagedBean
 import org.goblinframework.core.service.GoblinManagedObject
 import org.springframework.context.ApplicationContext
 
 @GoblinManagedBean(type = "core")
 class SpringContainer(val applicationContext: ApplicationContext)
-  : GoblinManagedObject(), SpringContainerMXBean, ApplicationContextProvider {
+  : GoblinManagedObject(), SpringContainerMXBean {
 
   override fun getUniqueId(): String {
     return (applicationContext as? SpringContainerId)?.uniqueId() ?: throw UnsupportedOperationException()
-  }
-
-  override fun applicationContext(): Any {
-    return applicationContext
   }
 }
