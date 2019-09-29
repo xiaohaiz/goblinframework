1982c55e9a72c8700cd5a87a6b19ef3d33972cf4
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-29 13:49:51 +0800

    org.goblinframework.core.util.ThreadUtils.awaitUninterruptibly

--- goblinframework-core/src/main/java/org/goblinframework/core/util/ThreadUtils.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/util/ThreadUtils.java
@@ -1,6 +1,9 @@
 package org.goblinframework.core.util;
 
 import org.goblinframework.core.concurrent.GoblinInterruptedException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.CountDownLatch;
 
 abstract public class ThreadUtils extends org.apache.commons.lang3.ThreadUtils {
 
@@ -12,4 +15,11 @@ abstract public class ThreadUtils extends org.apache.commons.lang3.ThreadUtils {
     }
   }
 
+  public static void awaitUninterruptibly(@NotNull final CountDownLatch latch) {
+    try {
+      latch.await();
+    } catch (InterruptedException ex) {
+      throw new GoblinInterruptedException(ex);
+    }
+  }
 }
