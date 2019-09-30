f9f062f8141d2be1c511993626a4c53795e2a5a0
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-09-30 15:38:00 +0800

    org.goblinframework.database.mongo.support.MongoPrimaryKeySupport

--- goblinframework-database-mongo/src/main/java/org/goblinframework/database/mongo/support/MongoConversionSupport.kt
+++ goblinframework-database-mongo/src/main/java/org/goblinframework/database/mongo/support/MongoConversionSupport.kt
@@ -1,3 +1,3 @@
 package org.goblinframework.database.mongo.support
 
-abstract class MongoConversionSupport<E, ID> : MongoNamespaceSupport<E, ID>()
+abstract class MongoConversionSupport<E, ID> : MongoPrimaryKeySupport<E, ID>()
---
+++ goblinframework-database-mongo/src/main/java/org/goblinframework/database/mongo/support/MongoPrimaryKeySupport.kt
@@ -0,0 +1,3 @@
+package org.goblinframework.database.mongo.support
+
+abstract class MongoPrimaryKeySupport<E, ID> : MongoNamespaceSupport<E, ID>()
