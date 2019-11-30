6290114839ebcccc2d22f4f329e06fe25312ec4d
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-11-30 16:20:25 +0800

    ...

--- goblinframework-example/goblinframework-example-remote/goblinframework-example-remote-server/src/main/java/org/goblinframework/example/remote/server/Server.kt
+++ goblinframework-example/goblinframework-example-remote/goblinframework-example-remote-server/src/main/java/org/goblinframework/example/remote/server/Server.kt
@@ -2,9 +2,18 @@ package org.goblinframework.example.remote.server
 
 import org.goblinframework.bootstrap.core.StandaloneServer
 import org.goblinframework.core.container.GoblinSpringContainer
+import org.goblinframework.core.container.SpringContainer
+import org.goblinframework.example.remote.server.service.EchoServiceImpl
+import org.goblinframework.remote.server.service.RemoteServiceManager
 
 @GoblinSpringContainer("/config/goblinframework-example-remote-server.xml")
-class Server : StandaloneServer()
+class Server : StandaloneServer() {
+
+  override fun doService(container: SpringContainer?) {
+    RemoteServiceManager.createDittoService(EchoServiceImpl.INSTANCE)
+  }
+
+}
 
 fun main(args: Array<String>) {
   Server().bootstrap(args)
---
+++ goblinframework-example/goblinframework-example-remote/goblinframework-example-remote-server/src/main/java/org/goblinframework/example/remote/server/service/TimeServiceImpl.kt
@@ -0,0 +1,22 @@
+package org.goblinframework.example.remote.server.service
+
+import org.goblinframework.api.concurrent.GoblinFuture
+import org.goblinframework.api.remote.ExposeService
+import org.goblinframework.core.container.SpringContainerObject
+import org.goblinframework.example.remote.api.TimeService
+import org.goblinframework.remote.core.ValueWrapperFuture
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+@ExposeService(interfaceClass = TimeService::class)
+class TimeServiceImpl : SpringContainerObject(), TimeService {
+
+  override fun currentTimeMillis(): Long {
+    return System.currentTimeMillis()
+  }
+
+  override fun currentDate(): GoblinFuture<Date> {
+    return ValueWrapperFuture(Date())
+  }
+}
