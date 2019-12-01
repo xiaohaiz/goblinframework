d48ec034beb4f9ccbf0723d48a74858cbc1e1849
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-01 17:06:56 +0800

    debug...

--- goblinframework-example/goblinframework-example-remote/goblinframework-example-remote-client/src/main/java/org/goblinframework/example/remote/client/Client.kt
+++ goblinframework-example/goblinframework-example-remote/goblinframework-example-remote-client/src/main/java/org/goblinframework/example/remote/client/Client.kt
@@ -3,12 +3,16 @@ package org.goblinframework.example.remote.client
 import org.goblinframework.bootstrap.core.StandaloneServer
 import org.goblinframework.core.container.GoblinSpringContainer
 import org.goblinframework.core.container.SpringContainer
+import org.goblinframework.example.remote.api.TimeService
+import org.goblinframework.remote.client.service.RemoteServiceClientManager
+import org.goblinframework.remote.core.service.RemoteServiceId
 
 @GoblinSpringContainer("/config/goblinframework-example-remote-client.xml")
 class Client : StandaloneServer() {
 
   override fun doService(container: SpringContainer?) {
-
+    val serviceId = RemoteServiceId(TimeService::class.java.name, "1.0.0")
+    RemoteServiceClientManager.INSTANCE.getRemoteService(serviceId)
   }
 }
 
