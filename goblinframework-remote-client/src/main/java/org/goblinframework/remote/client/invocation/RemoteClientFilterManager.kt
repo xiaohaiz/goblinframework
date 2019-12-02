71aedd5717b257e7f6c5b8df1e897d30ad77701b
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-02 16:19:33 +0800

    org.goblinframework.remote.client.invocation.filter.SelectRouterFilter

--- goblinframework-remote-client/src/main/java/org/goblinframework/remote/client/invocation/RemoteClientFilterManager.kt
+++ goblinframework-remote-client/src/main/java/org/goblinframework/remote/client/invocation/RemoteClientFilterManager.kt
@@ -7,6 +7,7 @@ import org.goblinframework.core.service.ServiceInstaller
 import org.goblinframework.remote.client.invocation.endpoint.RemoteClientInvocationEndpoint
 import org.goblinframework.remote.client.invocation.filter.CreateFlightFilter
 import org.goblinframework.remote.client.invocation.filter.EncodeRequestFilter
+import org.goblinframework.remote.client.invocation.filter.SelectRouterFilter
 
 @Singleton
 @GoblinManagedBean("RemoveClient")
@@ -30,6 +31,7 @@ class RemoteClientFilterManager private constructor()
     val filters = mutableListOf<RemoteClientFilter>()
     filters.add(EncodeRequestFilter.INSTANCE)
     filters.add(CreateFlightFilter.INSTANCE)
+    filters.add(SelectRouterFilter.INSTANCE)
     filters.addAll(customized)
     filters.add(RemoteClientInvocationEndpoint.INSTANCE)
     return RemoteClientFilterChainImpl(filters)
@@ -38,6 +40,7 @@ class RemoteClientFilterManager private constructor()
   override fun disposeBean() {
     RemoteClientInvocationEndpoint.INSTANCE.dispose()
     customized.sortedByDescending { it.order }.forEach { it.dispose() }
+    SelectRouterFilter.INSTANCE.dispose()
     CreateFlightFilter.INSTANCE.dispose()
     EncodeRequestFilter.INSTANCE.dispose()
   }
