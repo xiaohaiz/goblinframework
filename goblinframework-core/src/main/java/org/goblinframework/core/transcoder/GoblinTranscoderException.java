33c9ef6562c7f7f7faee4d520d2ac72dc89f18ef
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-02 21:54:20 +0800

    org.goblinframework.remote.client.dispatcher.response.RemoteClientResponseDecoder

--- goblinframework-core/src/main/java/org/goblinframework/core/transcoder/GoblinTranscoderException.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/transcoder/GoblinTranscoderException.java
@@ -1,6 +1,7 @@
 package org.goblinframework.core.transcoder;
 
 import org.goblinframework.api.core.GoblinException;
+import org.jetbrains.annotations.NotNull;
 
 public class GoblinTranscoderException extends GoblinException {
   private static final long serialVersionUID = -480279730643006921L;
@@ -18,5 +19,14 @@ public class GoblinTranscoderException extends GoblinException {
 
   public GoblinTranscoderException(Throwable cause) {
     super(cause);
+  }
+
+  @NotNull
+  public static GoblinTranscoderException rethrow(@NotNull Throwable cause) {
+    if (cause instanceof GoblinTranscoderException) {
+      return (GoblinTranscoderException) cause;
+    } else {
+      return new GoblinTranscoderException(cause);
+    }
   }
 }
--- goblinframework-remote-client/src/main/java/org/goblinframework/remote/client/dispatcher/response/RemoteClientResponseDecoder.kt
+++
@@ -1,9 +0,0 @@
-package org.goblinframework.remote.client.dispatcher.response
-
-import org.goblinframework.core.service.GoblinManagedBean
-import org.goblinframework.core.service.GoblinManagedObject
-
-@GoblinManagedBean("RemoteClient")
-class RemoteClientResponseDecoder internal constructor()
-  : GoblinManagedObject(), RemoteClientResponseDecoderMXBean {
-}
