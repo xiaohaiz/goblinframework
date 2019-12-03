8feb7b3ef57161e50aee86366f8cc4b2edba359d
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-03 21:32:22 +0800

    channelNotFound listenerNotFound

--- goblinframework-core/src/test/java/org/goblinframework/core/event/EventBusTest.kt
+++ goblinframework-core/src/test/java/org/goblinframework/core/event/EventBusTest.kt
@@ -1,9 +1,11 @@
 package org.goblinframework.core.event
 
+import org.goblinframework.core.event.exception.EventBossChannelNotFoundException
+import org.goblinframework.core.event.exception.EventBossListenerNotFoundException
+import org.goblinframework.core.event.exception.EventBusException
 import org.goblinframework.core.util.RandomUtils
 import org.goblinframework.test.runner.GoblinTestRunner
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.test.context.ContextConfiguration
@@ -11,6 +13,38 @@ import org.springframework.test.context.ContextConfiguration
 @RunWith(GoblinTestRunner::class)
 @ContextConfiguration("/UT.xml")
 class EventBusTest {
+
+  class EventBusTestEvent : GoblinEvent()
+
+  @Test
+  fun channelNotFound() {
+    val channel = "/${RandomUtils.nextObjectId()}"
+    val future = EventBus.publish(channel, EventBusTestEvent())
+    try {
+      future.get()
+      fail()
+    } catch (ex: EventBusException) {
+      assertTrue(ex.cause is EventBossChannelNotFoundException)
+    }
+  }
+
+  @Test
+  fun listenerNotFound() {
+    val channel = "/${RandomUtils.nextObjectId()}"
+    EventBus.register(channel, 32, 1)
+    try {
+      val future = EventBus.publish(channel, EventBusTestEvent())
+      try {
+        future.get()
+        fail()
+      } catch (ex: EventBusException) {
+        assertTrue(ex.cause is EventBossListenerNotFoundException)
+      }
+    } finally {
+      EventBus.unregister(channel)
+    }
+  }
+
   @Test
   fun execute() {
     val s = RandomUtils.randomAlphanumeric(64)
