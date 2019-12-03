546ccdfd89c81acac607f4a93d018959b8dcd9f7
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-03 17:09:50 +0800

    org.goblinframework.core.event.exception.EventWorkerBufferFullException

--- goblinframework-core/src/main/java/org/goblinframework/core/event/exception/EventWorkerBufferFullException.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/event/exception/EventWorkerBufferFullException.java
@@ -1,5 +1,5 @@
 package org.goblinframework.core.event.exception;
 
-public class EventWorkerBufferFullException extends EventException {
+public class EventWorkerBufferFullException extends EventWorkerException {
   private static final long serialVersionUID = 30892987215049726L;
 }
