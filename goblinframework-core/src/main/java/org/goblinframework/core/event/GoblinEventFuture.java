4dac1601fb4bc6e378f3d71673f46f18a8d246b4
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-03 20:49:03 +0800

    org.goblinframework.core.event.GoblinEventFuture.addDiscardListener

--- goblinframework-core/src/main/java/org/goblinframework/core/event/GoblinEventFuture.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/event/GoblinEventFuture.java
@@ -1,6 +1,8 @@
 package org.goblinframework.core.event;
 
 import org.goblinframework.api.concurrent.GoblinFuture;
+import org.goblinframework.api.function.Block0;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Goblin event future abstraction.
@@ -9,4 +11,9 @@ import org.goblinframework.api.concurrent.GoblinFuture;
  * @since Dec 3, 2019
  */
 public interface GoblinEventFuture extends GoblinFuture<GoblinEventContext> {
+
+  /**
+   * Callback the specified action in case of event future discarded.
+   */
+  void addDiscardListener(@NotNull Block0 action);
 }
--- goblinframework-core/src/main/java/org/goblinframework/core/event/context/GoblinEventFutureImpl.kt
+++ goblinframework-core/src/main/java/org/goblinframework/core/event/context/GoblinEventFutureImpl.kt
@@ -1,6 +1,7 @@
 package org.goblinframework.core.event.context
 
 import org.goblinframework.api.concurrent.GoblinFuture
+import org.goblinframework.api.function.Block0
 import org.goblinframework.core.concurrent.GoblinFutureImpl
 import org.goblinframework.core.event.GoblinEventContext
 import org.goblinframework.core.event.GoblinEventFuture
@@ -8,8 +9,11 @@ import org.goblinframework.core.event.exception.EventBusException
 
 class GoblinEventFutureImpl : GoblinFutureImpl<GoblinEventContext>(), GoblinEventFuture {
 
+  private var context: GoblinEventContextImpl? = null
+
   override fun complete(result: GoblinEventContext?): GoblinFuture<GoblinEventContext> {
     val context = result as GoblinEventContextImpl
+    this.context = context
     if (context.isSuccess) {
       return super.complete(result)
     }
@@ -23,4 +27,13 @@ class GoblinEventFutureImpl : GoblinFutureImpl<GoblinEventContext>(), GoblinEven
     return super.complete(result, error)
   }
 
+  override fun addDiscardListener(action: Block0) {
+    addListener {
+      context?.run {
+        if (this.isDiscard) {
+          action.apply()
+        }
+      }
+    }
+  }
 }
