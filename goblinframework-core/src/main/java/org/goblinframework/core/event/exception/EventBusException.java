f4cb52c6422dbae5d8bacad2d80c6250522bdef7
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-03 21:26:15 +0800

    org.goblinframework.core.event.exception.EventBusException

--- goblinframework-core/src/main/java/org/goblinframework/core/event/exception/EventBusException.java
+++ goblinframework-core/src/main/java/org/goblinframework/core/event/exception/EventBusException.java
@@ -8,8 +8,37 @@ import java.util.Map;
 public class EventBusException extends EventException {
   private static final long serialVersionUID = -7710045073339017579L;
 
+  @Nullable private final EventBossException bossException;
+  @NotNull private final Map<Integer, EventWorkerException> workerExceptions;
+  private final int taskCount;
+
   public EventBusException(@Nullable EventBossException bossException,
                            @NotNull Map<Integer, EventWorkerException> workerExceptions,
                            int taskCount) {
+    this.bossException = bossException;
+    this.workerExceptions = workerExceptions;
+    this.taskCount = taskCount;
+  }
+
+  @Nullable
+  public EventBossException getBossException() {
+    return bossException;
+  }
+
+  @NotNull
+  public Map<Integer, EventWorkerException> getWorkerExceptions() {
+    return workerExceptions;
+  }
+
+  public int getTaskCount() {
+    return taskCount;
+  }
+
+  @Override
+  public synchronized Throwable getCause() {
+    if (bossException != null) {
+      return bossException;
+    }
+    return workerExceptions.values().stream().findFirst().orElse(null);
   }
 }
