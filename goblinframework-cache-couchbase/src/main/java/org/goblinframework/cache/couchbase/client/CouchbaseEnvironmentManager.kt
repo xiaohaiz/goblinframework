6321e37fb3ac814bff538b78f11f8c70c52417f3
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-04 14:01:04 +0800

    org.goblinframework.cache.couchbase.client.CouchbaseEnvironmentManager

---
+++ goblinframework-cache-couchbase/src/main/java/org/goblinframework/cache/couchbase/client/CouchbaseEnvironmentManager.kt
@@ -0,0 +1,41 @@
+package org.goblinframework.cache.couchbase.client
+
+import com.couchbase.client.java.env.CouchbaseEnvironment
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment
+import org.goblinframework.api.annotation.Singleton
+import org.goblinframework.api.annotation.ThreadSafe
+import org.goblinframework.core.service.GoblinManagedBean
+import org.goblinframework.core.service.GoblinManagedObject
+import java.util.concurrent.atomic.AtomicReference
+
+@Singleton
+@ThreadSafe
+@GoblinManagedBean("CacheCouchbase")
+class CouchbaseEnvironmentManager private constructor()
+  : GoblinManagedObject(), CouchbaseEnvironmentManagerMXBean {
+
+  companion object {
+    @JvmField val INSTANCE = CouchbaseEnvironmentManager()
+  }
+
+  private val environmentReference = AtomicReference<CouchbaseEnvironment?>()
+
+  @Synchronized
+  fun getCouchbaseEnvironment(): CouchbaseEnvironment {
+    environmentReference.get()?.run {
+      return this
+    }
+    val environment = DefaultCouchbaseEnvironment.builder()
+        .dnsSrvEnabled(false)
+        .build()
+    environmentReference.set(environment)
+    return environment
+  }
+
+  @Synchronized
+  override fun disposeBean() {
+    environmentReference.getAndSet(null)?.run {
+      this.shutdownAsync().toBlocking().single()
+    }
+  }
+}
--- goblinframework-cache-couchbase/src/main/java/org/goblinframework/cache/couchbase/module/CacheCouchbaseModule.kt
+++ goblinframework-cache-couchbase/src/main/java/org/goblinframework/cache/couchbase/module/CacheCouchbaseModule.kt
@@ -1,13 +1,24 @@
 package org.goblinframework.cache.couchbase.module
 
 import org.goblinframework.api.annotation.Install
+import org.goblinframework.cache.couchbase.client.CouchbaseEnvironmentManager
 import org.goblinframework.core.system.GoblinSubModule
 import org.goblinframework.core.system.ISubModule
+import org.goblinframework.core.system.ModuleFinalizeContext
+import org.goblinframework.core.system.ModuleInitializeContext
 
 @Install
 class CacheCouchbaseModule : ISubModule {
 
   override fun id(): GoblinSubModule {
     return GoblinSubModule.CACHE_COUCHBASE
+  }
+
+  override fun initialize(ctx: ModuleInitializeContext) {
+    CouchbaseEnvironmentManager.INSTANCE.initialize()
+  }
+
+  override fun finalize(ctx: ModuleFinalizeContext) {
+    CouchbaseEnvironmentManager.INSTANCE.dispose()
   }
 }
