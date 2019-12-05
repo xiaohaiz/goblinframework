b35bbc40b3713f965af62d37f2efbfa4e4dcb0c4
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-05 17:57:12 +0800

    org.goblinframework.dao.mongo.bson.introspect.StringIdSerializer

--- goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/bson/introspect/StringIdSerializer.java
+++ goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/bson/introspect/StringIdSerializer.java
@@ -1,4 +1,4 @@
-package org.goblinframework.database.mongo.bson.introspect;
+package org.goblinframework.dao.mongo.bson.introspect;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
