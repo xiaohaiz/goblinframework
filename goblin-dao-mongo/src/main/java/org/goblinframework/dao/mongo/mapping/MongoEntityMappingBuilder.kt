f5365beab029897bb71e658fad0b84a9a9a65581
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-07 09:24:43 +0800

    org.goblinframework.dao.mysql.mapping.MysqlEntityMappingBuilder

--- goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/mapping/MongoEntityMappingBuilder.kt
+++ goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/mapping/MongoEntityMappingBuilder.kt
@@ -1,10 +1,12 @@
 package org.goblinframework.dao.mongo.mapping
 
+import org.goblinframework.api.annotation.Singleton
 import org.goblinframework.core.service.GoblinManagedBean
 import org.goblinframework.database.core.mapping.EntityMappingBuilder
 import org.goblinframework.database.mongo.mapping.MongoEntityFieldNameResolver
 import org.goblinframework.database.mongo.mapping.MongoEntityFieldScanner
 
+@Singleton
 @GoblinManagedBean("MongoDao")
 class MongoEntityMappingBuilder private constructor()
   : EntityMappingBuilder(MongoEntityFieldScanner.INSTANCE, MongoEntityFieldNameResolver.INSTANCE) {
---
+++ goblin-dao-mysql/src/main/java/org/goblinframework/dao/mysql/mapping/MysqlEntityMappingBuilder.kt
@@ -0,0 +1,17 @@
+package org.goblinframework.dao.mysql.mapping
+
+import org.goblinframework.api.annotation.Singleton
+import org.goblinframework.core.service.GoblinManagedBean
+import org.goblinframework.database.core.mapping.EntityMappingBuilder
+import org.goblinframework.database.mysql.mapping.MysqlEntityFieldNameResolver
+import org.goblinframework.database.mysql.mapping.MysqlEntityFieldScanner
+
+@Singleton
+@GoblinManagedBean("MysqlDao")
+class MysqlEntityMappingBuilder private constructor()
+  : EntityMappingBuilder(MysqlEntityFieldScanner.INSTANCE, MysqlEntityFieldNameResolver.INSTANCE) {
+
+  companion object {
+    val INSTANCE = MysqlEntityMappingBuilder()
+  }
+}
