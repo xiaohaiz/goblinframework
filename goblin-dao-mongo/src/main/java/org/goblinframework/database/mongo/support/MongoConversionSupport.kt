c3af0757f0b652be2f8639dd367b35dd3b424e4b
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-08 17:08:51 +0800

    Remove unused class.

--- goblin-dao-mongo/src/main/java/org/goblinframework/database/mongo/support/MongoConversionSupport.kt
+++ goblin-dao-mongo/src/main/java/org/goblinframework/database/mongo/support/MongoConversionSupport.kt
@@ -1,9 +1,10 @@
 package org.goblinframework.database.mongo.support
 
 import org.bson.BsonDocument
+import org.goblinframework.dao.mongo.persistence.internal.MongoPersistencePrimaryKeySupport
 import org.goblinframework.database.mongo.bson.BsonConversionService
 
-abstract class MongoConversionSupport<E, ID> : MongoPrimaryKeySupport<E, ID>() {
+abstract class MongoConversionSupport<E, ID> : MongoPersistencePrimaryKeySupport<E, ID>() {
 
   fun convertBsonDocument(doc: BsonDocument?): E? {
     return doc?.run {
