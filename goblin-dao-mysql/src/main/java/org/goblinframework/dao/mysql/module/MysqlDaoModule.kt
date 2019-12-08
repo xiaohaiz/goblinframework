e8f8076b1b45cbe08370bb7d8511caa1fe1068b8
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-08 14:42:18 +0800

    org.goblinframework.dao.mysql.module.MysqlDaoModule

--- goblin-dao-mysql/src/main/java/org/goblinframework/dao/mysql/module/MysqlDaoModule.kt
+++ goblin-dao-mysql/src/main/java/org/goblinframework/dao/mysql/module/MysqlDaoModule.kt
@@ -2,6 +2,7 @@ package org.goblinframework.dao.mysql.module
 
 import org.goblinframework.api.annotation.Install
 import org.goblinframework.core.system.*
+import org.goblinframework.dao.mysql.module.test.RebuildTableBeforeTestMethod
 import org.goblinframework.database.mysql.client.MysqlClientManager
 import org.goblinframework.database.mysql.module.config.MysqlConfigManager
 import org.goblinframework.database.mysql.module.test.RebuildMysqlTableBeforeTestMethod
@@ -14,6 +15,7 @@ class MysqlDaoModule : ISubModule {
   }
 
   override fun install(ctx: ModuleInstallContext) {
+    ctx.registerTestExecutionListener(RebuildTableBeforeTestMethod.getInstance())
     ctx.registerTestExecutionListener(RebuildMysqlTableBeforeTestMethod.INSTANCE)
   }
 
