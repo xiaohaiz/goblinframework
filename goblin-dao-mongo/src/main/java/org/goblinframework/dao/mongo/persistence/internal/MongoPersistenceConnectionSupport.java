d051d3422d0f0cef85aa21a0c8a56f34e9874b04
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-08 16:20:15 +0800

    org.goblinframework.dao.mongo.persistence.internal.MongoPersistenceConnectionSupport

---
+++ goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/persistence/internal/MongoPersistenceConnectionSupport.java
@@ -0,0 +1,25 @@
+package org.goblinframework.dao.mongo.persistence.internal;
+
+import org.goblinframework.core.util.AnnotationUtils;
+import org.goblinframework.dao.annotation.PersistenceConnection;
+import org.goblinframework.dao.mongo.exception.GoblinMongoPersistenceException;
+import org.goblinframework.database.mongo.client.MongoClient;
+import org.goblinframework.database.mongo.client.MongoClientManager;
+
+abstract public class MongoPersistenceConnectionSupport<E, ID> extends MongoPersistenceEntityMappingSupport<E, ID> {
+
+  protected final com.mongodb.reactivestreams.client.MongoClient mongoClient;
+
+  protected MongoPersistenceConnectionSupport() {
+    PersistenceConnection annotation = AnnotationUtils.getAnnotation(getClass(), PersistenceConnection.class);
+    if (annotation == null) {
+      throw new GoblinMongoPersistenceException("No @PersistenceConnection presented");
+    }
+    String connection = annotation.connection();
+    MongoClient client = MongoClientManager.INSTANCE.getMongoClient(connection);
+    if (client == null) {
+      throw new GoblinMongoPersistenceException("MongoClient [" + connection + "] not found");
+    }
+    this.mongoClient = client.getNativeClient();
+  }
+}
