e01e46019eda11b72aec7de029d6b01c4670eae0
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-09 21:39:19 +0800

    org.goblinframework.embedded.jetty.server.JettyEmbeddedServerImpl

--- goblin-embedded-jetty/src/main/java/org/goblinframework/embedded/jetty/server/JettyEmbeddedServerImpl.kt
+++ goblin-embedded-jetty/src/main/java/org/goblinframework/embedded/jetty/server/JettyEmbeddedServerImpl.kt
@@ -1,10 +1,53 @@
 package org.goblinframework.embedded.jetty.server
 
+import org.eclipse.jetty.server.GoblinHttpConnectionFactory
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
+import org.eclipse.jetty.server.handler.ContextHandler
+import org.eclipse.jetty.server.handler.ContextHandlerCollection
+import org.eclipse.jetty.server.handler.gzip.GzipHandler
+import org.eclipse.jetty.util.thread.QueuedThreadPool
 import org.goblinframework.api.function.Disposable
+import org.goblinframework.core.util.ClassUtils
 import org.goblinframework.embedded.core.setting.ServerSetting
+import java.util.concurrent.TimeUnit
 
 class JettyEmbeddedServerImpl(private val setting: ServerSetting) : Disposable {
 
+  private val server: Server
+
+  init {
+    val maxThreads = setting.threadPoolSetting().maximumPoolSize()
+    val minThreads = setting.threadPoolSetting().corePoolSize()
+    val unit = setting.threadPoolSetting().unit()
+    val time = setting.threadPoolSetting().keepAliveTime()
+    val idleTimeout = TimeUnit.MILLISECONDS.convert(time, unit)
+    val threadPool = QueuedThreadPool(maxThreads, minThreads, idleTimeout.toInt())
+    server = Server(threadPool)
+    val connector = ServerConnector(server)
+    connector.connectionFactories = listOf(GoblinHttpConnectionFactory())
+    connector.host = setting.networkSetting().host()
+    connector.port = setting.networkSetting().port()
+    server.addConnector(connector)
+
+    val handlers = ContextHandlerCollection()
+    setting.handlerSettings().values.forEach {
+      val contextHandler = ContextHandler()
+      contextHandler.contextPath = it.contextPath()
+      contextHandler.resourceBase = "."
+      contextHandler.classLoader = ClassUtils.getDefaultClassLoader()
+      contextHandler.isCompactPath = true
+      contextHandler.handler = JettyHttpRequestHandler(it.servletHandler())
+      val gzipHandler = GzipHandler()
+      gzipHandler.handler = contextHandler
+      handlers.addHandler(gzipHandler)
+    }
+    server.handler = handlers
+    server.start()
+
+  }
+
   override fun dispose() {
+    server.stop()
   }
 }
---
+++ goblin-embedded-jetty/src/main/java/org/goblinframework/embedded/jetty/server/JettyHttpRequestHandler.java
@@ -0,0 +1,48 @@
+package org.goblinframework.embedded.jetty.server;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.goblinframework.core.util.HttpUtils;
+import org.goblinframework.embedded.core.handler.ServletHandler;
+import org.goblinframework.webmvc.servlet.ServletRequest;
+import org.goblinframework.webmvc.servlet.ServletResponse;
+import org.goblinframework.webmvc.util.HttpContentTypes;
+import org.jetbrains.annotations.NotNull;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+final public class JettyHttpRequestHandler extends AbstractHandler {
+
+  @NotNull private final ServletHandler handler;
+
+  JettyHttpRequestHandler(@NotNull ServletHandler handler) {
+    this.handler = handler;
+  }
+
+  @Override
+  public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    if (HttpUtils.isMalformedPath(target)) {
+      response.resetBuffer();
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().println("Malformed request path specified");
+      response.setContentType(HttpContentTypes.TEXT_PLAIN);
+      response.flushBuffer();
+      return;
+    }
+    try {
+      ServletRequest servletRequest = new ServletRequest(request);
+      ServletResponse servletResponse = new ServletResponse(response);
+      handler.handle(servletRequest, servletResponse);
+    } catch (Exception ex) {
+      response.resetBuffer();
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      response.setContentType(HttpContentTypes.TEXT_PLAIN);
+      ex.printStackTrace(response.getWriter());
+    } finally {
+      response.flushBuffer();
+    }
+  }
+}
