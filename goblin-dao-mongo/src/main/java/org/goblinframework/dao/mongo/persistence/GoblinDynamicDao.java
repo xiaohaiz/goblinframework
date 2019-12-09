94c764425b11cad3aaa12b69d2fb8263b390bc9f
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-09 14:38:36 +0800

    org.goblinframework.dao.mongo.persistence.GoblinDynamicDao

--- goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/persistence/GoblinDynamicDao.java
+++ goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/persistence/GoblinDynamicDao.java
@@ -1,7 +1,9 @@
 package org.goblinframework.dao.mongo.persistence;
 
+import com.mongodb.MongoNamespace;
 import org.goblinframework.dao.mongo.exception.GoblinMongoPersistenceException;
 import org.goblinframework.dao.mongo.persistence.internal.MongoPersistenceOperationSupport;
+import org.jetbrains.annotations.NotNull;
 
 abstract public class GoblinDynamicDao<E, ID> extends MongoPersistenceOperationSupport<E, ID> {
 
@@ -11,4 +13,17 @@ abstract public class GoblinDynamicDao<E, ID> extends MongoPersistenceOperationS
     }
   }
 
+  @NotNull
+  final protected MongoNamespace generateEntityNamespace(@NotNull E entity) {
+    String database = getEntityDatabaseName(entity);
+    String collection = getEntityCollectionName(entity);
+    return new MongoNamespace(database, collection);
+  }
+
+  @NotNull
+  final protected MongoNamespace generateIdNamespace(@NotNull ID id) {
+    String database = getIdDatabaseName(id);
+    String collection = getIdCollectionName(id);
+    return new MongoNamespace(database, collection);
+  }
 }
