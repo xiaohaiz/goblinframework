59699aae7ac9a72879cb944d57238b3c3268d7dd
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-09 16:54:40 +0800

    org.goblinframework.dao.mongo.module.monitor.translator.DropDatabaseOperationTranslator

--- goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/module/monitor/translator/OperationTranslatorFactory.java
+++ goblin-dao-mongo/src/main/java/org/goblinframework/dao/mongo/module/monitor/translator/OperationTranslatorFactory.java
@@ -2,6 +2,7 @@ package org.goblinframework.dao.mongo.module.monitor.translator;
 
 import com.mongodb.operation.AsyncReadOperation;
 import com.mongodb.operation.AsyncWriteOperation;
+import com.mongodb.operation.DropDatabaseOperation;
 import com.mongodb.operation.ListDatabasesOperation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,6 +20,9 @@ final public class OperationTranslatorFactory {
 
   @Nullable
   public static OperationTranslator createTranslator(@NotNull AsyncWriteOperation<?> writeOperation) {
+    if (writeOperation instanceof DropDatabaseOperation) {
+      return new DropDatabaseOperationTranslator((DropDatabaseOperation) writeOperation);
+    }
     return null;
   }
 }
