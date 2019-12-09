c9ddacaa1182cec7a3837311cb11578ed9b06d6b
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-09 18:26:41 +0800

    GoblinHttpConnectionFactory & GoblinHttpConnection

---
+++ goblin-embedded-jetty/src/main/java/org/eclipse/jetty/server/GoblinHttpConnection.java
@@ -0,0 +1,28 @@
+package org.eclipse.jetty.server;
+
+import org.eclipse.jetty.http.HttpCompliance;
+import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.io.EndPoint;
+import org.goblinframework.api.annotation.Compatible;
+import org.goblinframework.core.util.HttpUtils;
+
+@Compatible(
+    group = "org.eclipse.jetty",
+    artifact = "jetty-server",
+    version = "9.4.24.v20191120")
+public class GoblinHttpConnection extends HttpConnection {
+
+  GoblinHttpConnection(HttpConfiguration config, Connector connector, EndPoint endPoint, HttpCompliance compliance, boolean recordComplianceViolations) {
+    super(config, connector, endPoint, compliance, recordComplianceViolations);
+  }
+
+  @Override
+  protected HttpChannelOverHttp newHttpChannel() {
+    return new HttpChannelOverHttp(this, getConnector(), getHttpConfiguration(), getEndPoint(), this) {
+      @Override
+      public boolean startRequest(String method, String uri, HttpVersion version) {
+        return super.startRequest(method, HttpUtils.compactContinuousSlashes(uri), version);
+      }
+    };
+  }
+}
