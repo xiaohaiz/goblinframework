9424960d0f57612a94ea0729fe1cb1bef4a52f0f
Xiaohai Zhang <xiaohai.zhang@17zuoye.com>
2019-12-09 14:17:32 +0800

    org.goblinframework.dao.mongo.persistence.GoblinStaticDao

--- goblin-example/goblin-example-dao/src/main/java/org/goblinframework/example/database/mongo/dao/MongoExampleDataDao.kt
+++ goblin-example/goblin-example-dao/src/main/java/org/goblinframework/example/database/mongo/dao/MongoExampleDataDao.kt
@@ -4,7 +4,7 @@ import org.bson.types.ObjectId
 import org.goblinframework.dao.annotation.PersistenceConnection
 import org.goblinframework.dao.mongo.annotation.MongoPersistenceCollection
 import org.goblinframework.dao.mongo.annotation.MongoPersistenceDatabase
-import org.goblinframework.dao.mongo.persistence.GoblinStaticDao1
+import org.goblinframework.dao.mongo.persistence.GoblinStaticDao
 import org.goblinframework.example.database.mongo.entity.MongoExampleData
 import org.springframework.stereotype.Repository
 
@@ -12,6 +12,4 @@ import org.springframework.stereotype.Repository
 @PersistenceConnection(connection = "example")
 @MongoPersistenceDatabase(database = "test")
 @MongoPersistenceCollection(collection = "mongo_example_data")
-class MongoExampleDataDao : GoblinStaticDao1<MongoExampleData, ObjectId>() {
-
-}
+class MongoExampleDataDao : GoblinStaticDao<MongoExampleData, ObjectId>()
