1da6230b227009bcd62147abd00b5af642219190
xiaohaiz <xiaohaiz@hotmail.com>
2019-12-16 15:05:11 +0800

    org.goblinframework.rpc.service._ServiceTimeoutKt.calculateServiceTimeout(java.lang.Class<?>, long)

--- goblin-rpc/src/main/java/org/goblinframework/rpc/service/_ServiceTimeout.kt
+++ goblin-rpc/src/main/java/org/goblinframework/rpc/service/_ServiceTimeout.kt
@@ -3,6 +3,13 @@ package org.goblinframework.rpc.service
 import org.goblinframework.api.rpc.ServiceTimeout
 import java.lang.reflect.Method
 
+fun calculateServiceTimeout(interfaceClass: Class<*>, defaultTimeout: Long): Long {
+  check(interfaceClass.isInterface)
+  val annotation = interfaceClass.getAnnotation(ServiceTimeout::class.java)
+  val timeout = calculateServiceTimeout(annotation) ?: return defaultTimeout
+  return if (timeout <= 0) defaultTimeout else timeout
+}
+
 fun calculateServiceTimeout(method: Method): Long? {
   val serviceTimeout = method.getAnnotation(ServiceTimeout::class.java)
   return calculateServiceTimeout(serviceTimeout)
