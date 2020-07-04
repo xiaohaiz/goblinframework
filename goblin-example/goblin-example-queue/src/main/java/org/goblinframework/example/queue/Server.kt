f6ac8de62351cab978b91ffb8d58a5412ce03540
changyuan.liu <changyuan.liu@17zuoye.com>
2020-07-04 20:44:01 +0800

    kafka producer

--- goblin-example/goblin-example-queue/src/main/java/org/goblinframework/example/queue/Server.kt
+++ goblin-example/goblin-example-queue/src/main/java/org/goblinframework/example/queue/Server.kt
@@ -4,12 +4,15 @@ import org.goblinframework.bootstrap.core.StandaloneServer
 import org.goblinframework.core.container.GoblinSpringContainer
 import org.goblinframework.core.container.SpringContainer
 import org.goblinframework.example.queue.producer.SampleProducer
+import org.goblinframework.queue.GoblinMessage
 
 @GoblinSpringContainer("/config/queue.xml")
 class Server : StandaloneServer() {
 
   override fun doService(container: SpringContainer?) {
     val producer = container!!.applicationContext.getBean(SampleProducer::class.java)
+
+    producer.getKafkaProducer().send(GoblinMessage.create().data("test"))
   }
 }
 
